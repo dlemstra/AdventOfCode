9426b53e149d12f253e46f2c2e18307b87a44788
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-02 00:17:46 +0100

    Implemented day 1.

---
+++ 2017/day/1/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestInverseCaptcha_1122(t *testing.T) {
+    result := InverseCaptcha("1122")
+    assert.Equal(t, 3, result)
+}
+
+func TestInverseCaptcha_1111(t *testing.T) {
+    result := InverseCaptcha("1111")
+    assert.Equal(t, 4, result)
+}
+
+func TestInverseCaptcha_1234(t *testing.T) {
+    result := InverseCaptcha("1234")
+    assert.Equal(t, 0, result)
+}
+
+func TestInverseCaptcha_91212129(t *testing.T) {
+    result := InverseCaptcha("91212129")
+    assert.Equal(t, 9, result)
+}
