cb813842ae507cb1c3a117e24786c274ba4f8ec8
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-02 10:28:52 +0100

    Start of day 2 part 2.

---
+++ 2017/day/2/part2/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestInverseCaptcha(t *testing.T) {
+    result := CorruptionChecksum(`5	9	2	8
+9	4	7	3
+3	8	6	5`)
+    assert.Equal(t, 9, result)
+}
