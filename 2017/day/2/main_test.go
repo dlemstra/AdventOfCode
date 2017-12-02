ab7edea8c1cd21153b41b5ce1c17bc759326086a
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-02 09:38:22 +0100

    Implemented day 2.

--- 2017/day/2/main_test.go
+++ 2017/day/2/main_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func TestInverseCaptcha(t *testing.T) {
-    result := CorruptionChecksum(`5 1 9 5
-7 5 3
-2 4 6 8`)
+    result := CorruptionChecksum(`5	1	9	5
+7	5	3
+2	4	6	8`)
     assert.Equal(t, 18, result)
 }
