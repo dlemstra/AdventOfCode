c0d334595d6bc6a3d1b1555c89b46b8de0461152
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-03 08:10:12 +0100

    Added extra test cases.

--- 2017/day/3/part1/main_test.go
+++ 2017/day/3/part1/main_test.go
@@ -15,9 +15,24 @@ func TestSpiralMemory_12(t *testing.T) {
     assert.Equal(t, 3, result)
 }
 
+func TestSpiralMemory_13(t *testing.T) {
+    result := SpiralMemory("12")
+    assert.Equal(t, 4, result)
+}
+
 func TestSpiralMemory_23(t *testing.T) {
     result := SpiralMemory("23")
     assert.Equal(t, 2, result)
+}
+
+func TestSpiralMemory_50(t *testing.T) {
+    result := SpiralMemory("50")
+    assert.Equal(t, 7, result)
+}
+
+func TestSpiralMemory_53(t *testing.T) {
+    result := SpiralMemory("53")
+    assert.Equal(t, 4, result)
 }
 
 func TestSpiralMemory_1024(t *testing.T) {
