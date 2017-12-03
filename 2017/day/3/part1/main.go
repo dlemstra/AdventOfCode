17c6e69677e8f82f18acb72620752e58458023bb
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-03 07:58:04 +0100

    Start of day 3.

---
+++ 2017/day/3/part1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+)
+
+func SpiralMemory(input string) int {
+    return 42
+}
+
+func main() {
+    result := SpiralMemory("325489")
+    fmt.Println(result)
+}
---
+++ 2017/day/3/part1/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestSpiralMemory_1(t *testing.T) {
+    result := SpiralMemory("1")
+    assert.Equal(t, 0, result)
+}
+
+func TestSpiralMemory_12(t *testing.T) {
+    result := SpiralMemory("12")
+    assert.Equal(t, 3, result)
+}
+
+func TestSpiralMemory_23(t *testing.T) {
+    result := SpiralMemory("23")
+    assert.Equal(t, 2, result)
+}
+
+func TestSpiralMemory_1024(t *testing.T) {
+    result := SpiralMemory("1024")
+    assert.Equal(t, 31, result)
+}
