b1aff776e571a1145c34b95b3486c2e0a9751f7f
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-04 07:15:19 +0100

    Implemented day 4 part 1.

--- 2017/day/4/part1/main.go
+++ 2017/day/4/part1/main.go
@@ -2,14 +2,41 @@ package main
 
 import (
     "fmt"
+    "strings"
+    "io/ioutil"
 )
 
+func appendIfMissing(slice []string, value string) []string {
+    for _, val := range slice {
+        if value == val {
+            return slice
+        }
+    }
+    return append(slice, value)
+}
+
 func IsValidPassphrase(passphrase string) (bool) {
-    return false
+    words := strings.Split(passphrase," ")
+    distinctWords := []string {}
+
+    for _, word := range words {
+        distinctWords = appendIfMissing(distinctWords, word)
+    }
+
+    return len(words) == len(distinctWords)
 }
 
 func HighEntropyPassphrases(fileName string) int {
-    return 1
+    dat, _ := ioutil.ReadFile(fileName)
+    passphrases := strings.Split(string(dat),"\n")
+
+    result := 0
+    for _, passphrase := range passphrases {
+        if (IsValidPassphrase(passphrase)) {
+            result++
+        }
+    }
+    return result
 }
 
 func main() {
