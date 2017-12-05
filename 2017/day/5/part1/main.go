3598e78f173c5e75a40d3b924d5ee98bd565b7ed
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-05 06:39:29 +0100

    Implemented day 5 part 1.

--- 2017/day/5/part1/main.go
+++ 2017/day/5/part1/main.go
@@ -2,14 +2,39 @@ package main
 
 import (
     "fmt"
+    "os"
+    "bufio"
+    "strconv"
 )
 
 func GetTwistyTrampolines(input []int) int {
-    return 42
+    steps := 0
+    i := 0
+    for i < len(input) {
+        steps++
+        inc := input[i]
+        if inc == 0 {
+            input[i]++
+            continue
+        }
+        input[i]++
+        i += inc
+    }
+    return steps
 }
 
 func TwistyTrampolines(fileName string) int {
-    return 42
+    file, _ := os.Open(fileName)
+    defer file.Close()
+
+    input := []int {}
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        val, _ := strconv.Atoi(scanner.Text())
+        input = append(input, val)
+    }
+
+    return GetTwistyTrampolines(input)
 }
 
 func main() {
