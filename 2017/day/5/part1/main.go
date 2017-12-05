fd55e49a6c110519cfc4ee0db3516affe270b56f
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-05 06:12:13 +0100

    Start of day 5.

---
+++ 2017/day/5/part1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+)
+
+func GetTwistyTrampolines(input []int) int {
+    return 42
+}
+
+func TwistyTrampolines(fileName string) int {
+    return 42
+}
+
+func main() {
+    result := TwistyTrampolines("../input")
+    fmt.Println(result)
+}
