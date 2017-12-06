31869de160a19f67bd26973caca8e1d55408ee5f
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-06 07:14:13 +0100

    Implemented day 6 part 1.

--- 2017/day/6/part1/main.go
+++ 2017/day/6/part1/main.go
@@ -7,14 +7,63 @@ import (
     "io/ioutil"
 )
 
+func max(input []int) (int, int) {
+    index := 0
+    max := 0
+    for i, val := range input {
+        if val > max {
+            index = i
+            max = val
+        }
+    }
+
+    return index, max
+}
+
+func checkSeen(seen [][]int, values []int) ([][]int, bool) {
+    for _, elem := range seen {
+        for i:=0; i < len(values); i++ {
+            if values[i] != elem[i] {
+                break
+            }
+            if i == len(values) - 1 {
+                return seen, true
+            }
+        }
+    }
+
+    item := make([]int, len(values))
+    copy(item, values)
+    seen = append(seen, item)
+    return seen, false
+}
+
 func MemoryReallocation(input []int) int {
-    return len(input)
+    cycles := 0
+    seen := [][]int {}
+    isSeen := false
+
+    for !isSeen {
+        index, max := max(input)
+        input[index] -= max
+        for i:=0; i < max; i++ {
+            index++
+            if index == len(input) {
+                index = 0
+            }
+            input[index]++
+        }
+        seen, isSeen = checkSeen(seen, input)
+        cycles++
+    }
+
+    return cycles
 }
 
 func ReadInput(fileName string) []int {
     dat, _ := ioutil.ReadFile(fileName)
     items := strings.Split(string(dat),"\t")
-    result := make([]int,len(items))
+    result := make([]int, len(items))
     for i, _ := range items {
         value, _ := strconv.Atoi(items[i])
         result[i] = value
