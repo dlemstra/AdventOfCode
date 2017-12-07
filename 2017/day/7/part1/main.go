0111d203e69f13b424e4b517caaeaff9a8ef8b99
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-07 19:37:19 +0100

    Implemented day 7 part 1.

--- 2017/day/7/part1/main.go
+++ 2017/day/7/part1/main.go
@@ -2,21 +2,74 @@ package main
 
 import (
     "fmt"
+    "os"
+    "bufio"
+    "strings"
+    "strconv"
 )
 
 type Tower struct {
     name string
     weight int
     childNames []string
-    children []Tower
+    parent *Tower
 }
 
-func RecursiveCircus(input []Tower) string {
-    return "42"
+func contains(items []string, item string) bool {
+    for _, a := range items {
+        if a == item {
+            return true
+        }
+    }
+    return false
+}
+
+func setParent(tower *Tower, towers []Tower) {
+    for _,elem := range towers {
+        if contains(elem.childNames, tower.name) {
+            tower.parent = &elem
+            return
+        }
+    }
+}
+
+func RecursiveCircus(towers []Tower) string {
+    for i,_ := range towers {
+        setParent(&towers[i], towers)
+    }
+
+    for _,tower := range towers {
+        if tower.parent == nil {
+            return tower.name
+        }
+    }
+
+    panic("should not be reached")
 }
 
 func ReadInput(fileName string) []Tower {
-    result := make([]Tower, 0)
+    file, _ := os.Open(fileName)
+    defer file.Close()
+
+    result := []Tower {}
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := strings.Split(scanner.Text(), " -> ")
+        start := strings.Split(line[0], " ")
+
+        name := start[0]
+        weight,_ := strconv.Atoi(start[1][1:len(start[1])-1])
+        childNames := []string {}
+        if (len(line) > 1) {
+            childNames = strings.Split(line[1], ", ")
+        }
+        tower := Tower {
+            name: name,
+            weight: weight,
+            childNames: childNames,
+        }
+        result = append(result, tower)
+    }
 
     return result
 }
