b36277571edce1d894e2db7ab87931a9eac42df9
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-08 06:39:47 +0100

    Implemented day 8 part 1.

--- 2017/day/8/part1/main.go
+++ 2017/day/8/part1/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
     "fmt"
+    "os"
+    "bufio"
+    "strings"
+    "strconv"
 )
 
 type Instruction struct {
@@ -12,12 +16,78 @@ type Instruction struct {
     compareValue int
 }
 
+func max(values map[string]int) int {
+    max := 0
+    for _, value := range values {
+        if (value > max) {
+            max = value
+        }
+    }
+
+    return max
+}
+
+func isConditionTrue(instruction Instruction, values map[string]int) bool {
+    compareValue := values[instruction.other]
+    switch compareType := instruction.compareType; compareType {
+        case ">":
+            return compareValue > instruction.compareValue
+        case ">=":
+            return compareValue >= instruction.compareValue
+        case "<":
+            return compareValue < instruction.compareValue
+        case "<=":
+            return compareValue <= instruction.compareValue
+        case "==":
+            return compareValue == instruction.compareValue
+        case "!=":
+            return compareValue != instruction.compareValue
+        default:
+            panic("unknown instruction")
+    }
+}
+
 func LikeRegisters(instructions []Instruction) int {
-    return 42
+    values := make(map[string]int)
+
+    for _,instruction := range instructions {
+        if isConditionTrue(instruction, values) {
+            values[instruction.target] += instruction.increment
+        }
+    }
+
+    return max(values)
 }
 
 func ReadInput(fileName string) []Instruction {
+    file, _ := os.Open(fileName)
+    defer file.Close()
+
     result := []Instruction {}
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        line := strings.Split(scanner.Text(), " if ")
+        start := strings.Split(line[0], " ")
+        end := strings.Split(line[1], " ")
+
+        target := start[0]
+        increment,_ := strconv.Atoi(start[2])
+        if (start[1] == "dec") {
+            increment *= -1
+        }
+        other := end[0]
+        compareType := end[1]
+        compareValue,_ := strconv.Atoi(end[2])
+
+        Instruction := Instruction {
+            target: target,
+            increment: increment,
+            other : other,
+            compareType: compareType,
+            compareValue: compareValue,
+        }
+        result = append(result, Instruction)
+    }
 
     return result
 }
