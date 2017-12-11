3a4ebac44edcfde0f7ddf32950486498b6f7e04b
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-11 11:43:03 +0100

    Implemented day 11 part 1.

---
+++ 2017/day/11/part1/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "strings"
+)
+
+func abs(a int) int {
+    if a < 0 {
+        return a * -1
+    }
+
+    return a
+}
+
+func max(a int, b int) int {
+    if a > b {
+        return a
+    }
+
+    return b
+}
+
+func HexEd(input []string) int {
+    x := 0
+    y := 0
+
+    for _, str := range input {
+        switch str {
+            case "n":
+                x--
+                y++
+            case "ne":
+                y++
+            case "se":
+                x++
+            case "s":
+                x++
+                y--
+            case "sw":
+                y--
+            case "nw":
+                x--
+        }
+    }
+
+    return max(abs(x), abs(y))
+}
+
+func ReadInput(fileName string) []string {
+    dat, _ := ioutil.ReadFile(fileName)
+    return strings.Split(strings.TrimSpace(string(dat)),",")
+}
+
+func main() {
+    input := ReadInput("../input")
+    result := HexEd(input)
+    fmt.Println(result)
+}
