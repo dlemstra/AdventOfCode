cffd10b27832a8b8e8b489da2e80ef2d18b768e8
Dirk Lemstra <dirk@git.imagemagick.org>
2017-12-21 23:59:46 +0100

    Implemented day 19 part 1.

---
+++ 2017/day/19/part1/main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestSeriesOfTubes(t *testing.T) {
+    input := ReadInput("../test_input")
+    result := SeriesOfTubes(input)
+    assert.Equal(t, "ABCDEF", result)
+}
