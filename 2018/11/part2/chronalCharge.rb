c4291a019aeb115eefdbb243974bd3f10081767a
Dirk Lemstra <dirk@lemstra.org>
2018-12-11 22:20:14 +0100

    Different implementation that uses: https://en.wikipedia.org/wiki/Summed-area_table

--- 2018/11/part2/chronalCharge.rb
+++ 2018/11/part2/chronalCharge.rb
@@ -1,4 +1,7 @@
 def powerLevel(x, y, serial)
+    if x == y || y  == 0
+        return 0
+    end
     rackId = x + 10
     powerLevel = rackId * y
     powerLevel += serial
@@ -11,57 +14,37 @@ def chronalCharge(serial)
 
     totals = {}
 
-    width = 300
-    grid = []
-    for y in 0..width-1
-        row = []
-        for x in 0..width-1
+    size = 300
+    grid = Array.new(size){Array.new(size) { 0 }}
+    for y in 1..size-1
+        for x in 1..size-1
             val = powerLevel(x, y, serial)
-            row.push(val)
+            val += grid[y][x-1]
+            val += grid[y-1][x]
+            val -= grid[y-1][x-1]
+            grid[y][x] = val
         end
-        grid.push(row)
     end
 
     max = 0
     maxPos = ""
-    minSize = 3
-    for y in 0..width-1
-        for x in 0..width-1
-            if x + minSize == width + 1
-                break
-            end
+    for y in 1..size-4
+        for x in 1..size-4
+            y0 = y - 1
+            x0 = x - 1
+            y1 = y + 2
+            x1 = x + 2
 
-            total = 0
-            for i in 0..minSize-1
-                for j in 0..minSize-1
-                    total += grid[y+i][x+j]
-                end
-            end
-
-            if total > max
-                max = total
-                maxPos = "#{x},#{y}"
-            end
-
-            start = [x,y].max
-
-            for step in minSize..width-start-1
-                for i in 0..step-1
-                    total += grid[y+i][x+step]
-                    total += grid[y+step][x+i]
-                end
-
-                total += grid[y+step][x+step]
-
+            while y1 < size-1 and x1 < size-1
+                y1 += 1
+                x1 += 1
+            
+                total = grid[y0][x0] + grid[y1][x1] - grid[y0][x1] - grid[y1][x0]
                 if total > max
                     max = total
-                    maxPos = "#{x},#{y},#{step+1}"
+                    maxPos = "#{x},#{y},#{x1-x+1}"
                 end
             end
-        end
-
-        if y + minSize == width
-            break
         end
     end
 
