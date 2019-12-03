084d16fca429f02ab007b87890162f30d1d5d615
Dirk Lemstra <dirk@lemstra.org>
2019-12-03 06:54:16 +0100

    Implemented day 3 part 1.

---
+++ 2019/03/part1/crossed-wires.kt
@@ -0,0 +1,45 @@
+package aoc
+
+import kotlin.math.abs
+import kotlin.math.min
+
+data class Position(val x: Int, val y: Int)
+
+fun getPositions(input: String): List<Position> {
+    var result = mutableListOf<Position>();
+
+    var x = 0;
+    var y = 0;
+
+    val movements = input.split(",").map{ it.trim() }
+    for (movement in movements) {
+        val direction = movement[0]
+        val steps = movement.substring(1).toInt()
+        for (i in 0 until steps) {
+            when (direction) {
+                'R' -> x++
+                'L' -> x--
+                'D' -> y++
+                'U' -> y--
+            }
+            result.add(Position(x, y))
+        }
+    }
+
+    return result;
+}
+
+fun crossedwWires(input: List<String>): Int {
+    val positions1 = getPositions(input[0]);
+    val positions2 = getPositions(input[1]);
+
+    val intersections = positions1.intersect(positions2)
+
+    var minDistance = Int.MAX_VALUE
+    for (intersection in intersections) {
+        val distance = abs(0 - intersection.x) + abs(0 - intersection.y)
+        minDistance = min(distance, minDistance)
+    }
+
+    return minDistance
+}
---
+++ 2019/03/part1/test.kt
@@ -0,0 +1,17 @@
+package aoc
+
+import kotlin.check
+
+class test() {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            checkEquals(6, crossedwWires(listOf("R8,U5,L5,D3", "U7,R6,D4,L4")))
+            checkEquals(159, crossedwWires(listOf("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")))
+            checkEquals(135, crossedwWires(listOf("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")))
+        }
+
+        fun checkEquals(expected: Int, actual: Int) {
+            check(expected == actual) { "Expected '$expected' but got '$actual'." }
+        }
+    }
+}
