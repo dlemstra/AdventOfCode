3ef567b3a35453735cf6272eb0f66fe16e464b99
Dirk Lemstra <dirk@lemstra.org>
2020-12-03 08:51:27 +0100

    Implemented day 3 part 2.

--- 2020/03/Part2.fs
+++ 2020/03/Part2.fs
@@ -1,5 +1,29 @@
 ﻿module Part2
 open Input
 
+let countTrees(inputs: seq<input>, stepSize: int, lineStepSize: int) =
+    let mutable trees = 0
+    let mutable offset = stepSize
+    let mutable lineNumber = lineStepSize
+
+    let length = (Seq.item 0 inputs).line.Length
+    let lineCount = Seq.length inputs
+    while lineNumber < lineCount do
+        let line = (Seq.item lineNumber inputs).line
+        if line.[offset % length] = '#' then
+            trees <- trees + 1
+        offset <- offset + stepSize
+        lineNumber <- lineNumber + lineStepSize
+
+    trees
+
 let execute(inputs: seq<input>) =
-    0
+    let mutable trees = 1
+
+    trees <- trees * countTrees(inputs, 1, 1)
+    trees <- trees * countTrees(inputs, 3, 1)
+    trees <- trees * countTrees(inputs, 5, 1)
+    trees <- trees * countTrees(inputs, 7, 1)
+    trees <- trees * countTrees(inputs, 1, 2)
+
+    trees
--- 2020/03/Program.fs
+++ 2020/03/Program.fs
@@ -10,5 +10,5 @@ let main argv =
     let inputs = readInput
 
     printfn "%d" (Part1.execute inputs)
-    //printfn "%d" (Part2.execute inputs)
+    printfn "%d" (Part2.execute inputs)
     0
