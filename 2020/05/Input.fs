682d4fc425b8e3371afbb3b1b2454b8013cd1cdd
Dirk Lemstra <dirk@lemstra.org>
2020-12-05 11:30:04 +0100

    Implemented day 5 part 1.

---
+++ 2020/05/Input.fs
@@ -0,0 +1,13 @@
+﻿module Input
+
+open System.IO
+
+type input(data: string) =
+    member this.data = data.ToCharArray()
+
+let read = seq<input> {
+    let lines = File.ReadAllLines("..\..\..\input")
+
+    for line in lines do
+        yield input(line)
+}
---
+++ 2020/05/Part1.fs
@@ -0,0 +1,19 @@
+﻿module Part1
+open Input
+
+let toInteger(value: char[], bitOne: char, start: int, stop: int) =
+    let mutable result = 0
+    for i in start .. stop do
+        if (value.[i] = bitOne) then
+            result <- result ||| (1 <<< stop-i)
+    result
+
+let getSeatId(input: input) =
+    let row = toInteger(input.data, 'B', 0, 6)
+    let column = toInteger(input.data, 'R', 7, 9)
+    row * 8 + column
+
+let execute(inputs: seq<input>) =
+    inputs
+        |> Seq.map getSeatId
+        |> Seq.max
