774aec1fd1d6470c9450cd90a9a60e60648606df
Dirk Lemstra <dirk@lemstra.org>
2020-12-08 17:58:46 +0100

    Implemented day 8 part 1.

---
+++ 2020/08/Part1.fs
@@ -0,0 +1,60 @@
+﻿module Part1
+
+open Input
+open System
+open System.Collections.Generic
+
+type IInstruction =
+    abstract member execute: input:int * int -> int * int
+
+type nop() = 
+    interface IInstruction with
+        member this.execute(position, value) =
+            (position + 1, value)
+
+type acc(increment: string) =
+    member this.increment = Int32.Parse(increment)
+    interface IInstruction with
+        member this.execute(position, value) =
+            (position + 1, value + this.increment)
+
+type jmp(increment: string) =
+    member this.increment = Int32.Parse(increment)
+    interface IInstruction with
+        member this.execute(position, value) =
+            (position + this.increment, value)
+
+let createInstruction(instruction: string): IInstruction =
+    let name = instruction.Substring(0, 3)
+    let value = instruction.Substring(3)
+
+    match name with
+        | "nop" -> new nop() :> IInstruction
+        | "acc" -> new acc(value) :> IInstruction
+        | "jmp" -> new jmp(value) :> IInstruction
+        | _ -> failwithf "Unknown instruction"
+
+let createInstructions(inputs: seq<input>) =
+    let result = new Dictionary<int, IInstruction>()
+
+    for i in 0 .. (Seq.length inputs) - 1 do
+        let instruction = createInstruction((Seq.item i inputs).instruction)
+        result.Add(i, instruction)
+
+    result
+
+let rec executeInstructions(instructions: Dictionary<int, IInstruction>, position: int, value: int) =
+    if instructions.ContainsKey(position) then
+        let instruction = instructions.[position]
+        instructions.Remove(position) |> ignore
+        let newPosition, newValue = instruction.execute(position, value)
+        executeInstructions(instructions, newPosition, newValue)
+    else
+        value
+
+
+let execute(inputs: seq<input>) =
+
+    let instructions = createInstructions(inputs)
+
+    executeInstructions(instructions, 0, 0)
---
+++ 2020/08/Program.fs
@@ -0,0 +1,7 @@
+﻿[<EntryPoint>]
+let main _ =
+    let inputs = Input.read
+
+    printfn "Part1: %d" (Part1.execute inputs)
+    printfn "Part2: %d" (Part2.execute inputs)
+    0
