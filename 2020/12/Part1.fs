5ea9d049bc0d32fb9624f1186499b9de05e091f4
Dirk Lemstra <dirk@lemstra.org>
2020-12-12 10:45:54 +0100

    Implemented day 12 part 1.

---
+++ 2020/12/Part1.fs
@@ -0,0 +1,63 @@
+﻿module Part1
+
+open Input
+open System
+
+type direction = North | East | South | West
+
+let rotateAngle(current: int, degrees: int) =
+    let newAngle = current + degrees
+    if newAngle >= 360 then
+        newAngle - 360
+    elif newAngle < 0 then
+        360 + newAngle
+    else
+        newAngle
+
+type ship() =
+    let mutable x = 0
+    let mutable y = 0
+    member this.position = (x, y)
+    member val direction = East with get, set
+    member this.move(steps: int, ?direction: direction) =
+        match defaultArg direction this.direction with
+            | North -> y <- y - steps
+            | East -> x <- x + steps
+            | South -> y <- y + steps
+            | West -> x <- x - steps
+    member this.rotate(degrees: int) =
+        let currentAngle = 
+            match this.direction with
+            | North -> 0
+            | East -> 90
+            | South -> 180
+            | West -> 270
+        this.direction <-
+            match rotateAngle(currentAngle, degrees) with
+            | 0 ->  North
+            | 90 -> East
+            | 180 -> South
+            | 270 -> West
+            | _ -> failwithf "Unknown direction"
+
+
+let execute(inputs: seq<input>) =
+    let ship = ship()
+
+    for input in inputs do
+        let action = input.instruction.[0]
+        let value = Int32.Parse(input.instruction.Substring(1))
+
+        match action with
+            | 'N' -> ship.move(value, North)
+            | 'E' -> ship.move(value, East)
+            | 'S' -> ship.move(value, South)
+            | 'W' -> ship.move(value, West)
+            | 'L' -> ship.rotate(-value)
+            | 'R' -> ship.rotate(value)
+            | 'F' -> ship.move(value)
+            | _ -> failwithf "Unknown instruction"
+
+    let x, y = ship.position
+    (abs x) + (abs y)
+
