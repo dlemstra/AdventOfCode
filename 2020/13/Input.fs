cba408b2ee154e4a61ab0f1ee940d880d1dc3bc7
Dirk Lemstra <dirk@lemstra.org>
2020-12-13 06:39:43 +0100

    Implemented day 13 part 1.

---
+++ 2020/13/Input.fs
@@ -0,0 +1,15 @@
+﻿module Input
+
+open System.IO
+open System
+
+type input(timestamp: int, ids: seq<int>) =
+    member this.timestamp = timestamp
+    member this.ids = ids
+
+let read =
+    let lines = File.ReadAllLines("..\..\..\input")
+
+    let timestamp = Int32.Parse lines.[0]
+    let ids = lines.[1].Split(',') |> Seq.filter(fun x -> x <> "x") |> Seq.map Int32.Parse
+    input(timestamp, ids)
---
+++ 2020/13/Part1.fs
@@ -0,0 +1,15 @@
+﻿module Part1
+
+open Input
+open System
+
+let execute(input: input) =
+    let mutable min = Int32.MaxValue
+    let mutable bestId = 0
+
+    for id in input.ids do
+        let duration =  id - (input.timestamp % id)
+        if duration < min then
+            min <- duration
+            bestId <- id
+    bestId * min
