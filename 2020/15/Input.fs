ecd5849dd3775bf43c55321c03ca4a1510e6ecb9
Dirk Lemstra <dirk@lemstra.org>
2020-12-15 07:03:42 +0100

    Implemented day 15 part 1.

---
+++ 2020/15/Input.fs
@@ -0,0 +1,13 @@
+﻿module Input
+
+open System
+open System.IO
+
+type input(numbers: seq<int>) =
+    member this.numbers = numbers
+
+let read =
+    let lines = File.ReadAllLines("..\..\..\input")
+
+    let numbers = lines.[0].Split(',') |> Seq.map Int32.Parse
+    input(numbers)
---
+++ 2020/15/Part1.fs
@@ -0,0 +1,34 @@
+﻿module Part1
+
+open Input
+open System.Collections.Generic
+
+type spoken = { mutable prev: int; mutable last: int }
+
+let execute(input: input) =
+    let numbers = new Dictionary<int, spoken>()
+    let mutable turn = 1
+
+    let mutable last = -1
+    for number in input.numbers do
+        last <- number
+        numbers.[last] <- { spoken.prev = turn; last = turn }
+        turn <- turn + 1
+
+    while turn <= 2020 do
+        let spoken = numbers.[last]
+
+        if spoken.prev = spoken.last then
+            last <- 0
+        else
+            last <- spoken.last - spoken.prev
+
+        if numbers.ContainsKey(last) then
+            numbers.[last].prev <- numbers.[last].last
+            numbers.[last].last <- turn
+        else
+            numbers.[last] <- { spoken.prev = turn; last = turn }
+
+        turn <- turn + 1
+
+    last
