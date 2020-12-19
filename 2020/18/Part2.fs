3b9f478f4b24f6d2cd81b2b5aaa13e5b96ad06d8
Dirk Lemstra <dirk@lemstra.org>
2020-12-19 08:41:48 +0100

    Implemented day 18 part 2.

--- 2020/18/Part2.fs
+++ 2020/18/Part2.fs
@@ -1,6 +1,82 @@
 ﻿module Part2
 
 open Input
+open System
+
+type valueType = Number | Add | Multiply
+
+type value(valueType: valueType, ?value: int64) =
+    member this.valueType = valueType
+    member this.value = defaultArg value -1L
+
+let evaluatePlus(values: list<value>) =
+    let mutable result = [values.[0]]
+
+    let mutable i = 1
+    while i < values.Length do
+        if values.[i].valueType = Add then
+            result <- result.[0 .. result.Length - 2] @ [value(Number, result.[result.Length - 1].value + values.[i + 1].value)]
+            i <- i + 2
+        else
+            result <- result @ [values.[i]]
+            i <- i + 1
+    result
+
+let evaluateMultiply(values: list<value>) =
+    let mutable result = values.[0].value
+    let mutable i = 1
+    while i < values.Length do
+        match values.[i].valueType with
+            | Multiply -> result <- result * values.[i + 1].value
+            | _ -> failwith "Invalid status"
+        i <- i + 2
+    result
+
+let getSubString(input: string) =
+    let mutable result = input.Substring(1, input.Length - 2)
+    let mutable i = 0
+    let mutable depth = 0
+    while i < result.Length do
+        if input.[i] = '(' then
+            depth <- depth + 1
+        elif input.[i] = ')' then
+            depth <- depth - 1
+            if depth = 0 then
+                result <- result.Substring(0, i - 1)
+                i <- result.Length
+        i <- i + 1
+    result
+
+let rec compute(input: string) =
+    let mutable values = []
+
+    let mutable number = -1L
+    let mutable i = 0
+    while i < input.Length do
+        match input.[i] with
+            | '(' ->
+                let subString = getSubString(input.Substring(i))
+                values <- values @ [value(Number, compute(subString))]
+                i <- i + subString.Length + 2
+            | '*' -> values <- values @ [value(Multiply)]
+            | '+' -> values <- values @ [value(Add)]
+            | ' ' ->
+                if number <> -1L then
+                    values <- values @ [value(Number, number)]
+                    number <- -1L
+            | _ ->
+                let value = Int64.Parse(input.[i].ToString())
+                if number = -1L then
+                    number <- value
+                else
+                    number <- (number * 10L) + value
+        i <- i + 1
+
+    if number <> -1L then
+        values <- values @ [value(Number, number)]
+
+    values <- evaluatePlus values
+    evaluateMultiply values
 
 let execute(inputs: seq<input>) =
-    0
+    inputs |> Seq.map (fun input -> compute input.line) |> Seq.sum
--- 2020/18/readme.md
+++ 2020/18/readme.md
@@ -38,3 +38,28 @@ Here are a few more examples:
    - `((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2` becomes 13632`.
 
 Before you can help with the homework, you need to understand it yourself. Evaluate the expression on each line of the homework; what is the sum of the resulting values?
+
+## --- Part Two ---
+
+You manage to answer the child's questions and they finish part 1 of their homework, but get stuck when they reach the next section: advanced math.
+
+Now, addition and multiplication have different precedence levels, but they're not the ones you're familiar with. Instead, addition is evaluated before multiplication.
+
+For example, the steps to evaluate the expression `1 + 2 * 3 + 4 * 5 + 6` are now as follows:
+
+```
+1 + 2 * 3 + 4 * 5 + 6
+  3   * 3 + 4 * 5 + 6
+  3   *   7   * 5 + 6
+  3   *   7   *  11
+     21       *  11
+         231
+```
+
+Here are the other examples from above:
+
+   - `1 + (2 * 3) + (4 * (5 + 6))` still becomes `51`.
+   - `2 * 3 + (4 * 5)` becomes `46`.
+   - `5 + (8 * 3 + 9 + 3 * 4 * 3)` becomes `1445`.
+   - `5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))` becomes `669060`.
+   - `((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2` becomes `23340`.
