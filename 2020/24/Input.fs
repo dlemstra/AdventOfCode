82f3036c888ffe53c8805cb958de97e9eff580ec
Dirk Lemstra <dirk@lemstra.org>
2020-12-24 08:28:16 +0100

    Implemented day 24 part 1.

---
+++ 2020/24/Input.fs
@@ -0,0 +1,18 @@
+﻿module Input
+
+open System.IO
+
+type input(directions: string) =
+    member this.directions = directions
+
+let read = seq {
+    let lines = File.ReadAllLines("..\..\..\input")
+    for line in lines do
+        let mutable temp = line.Replace("se","SE,")
+        temp <- temp.Replace("sw","SW,")
+        temp <- temp.Replace("nw","NW,")
+        temp <- temp.Replace("ne","NE,")
+        temp <- temp.Replace("w","w,")
+        temp <- temp.Replace("e","e,")
+        yield input(temp.ToLowerInvariant().TrimEnd(','))
+}
---
+++ 2020/24/Part1.fs
@@ -0,0 +1,27 @@
+﻿module Part1
+
+open Input
+open System.Collections.Generic
+
+type position = { x: int; y: int }
+
+let execute(inputs: input seq) =
+    let tiles = new Dictionary<position, char>()
+
+    for input in inputs do
+        let mutable pos = { x = 0; y = 0 }
+        for direction in input.directions.Split(',') do
+            match direction with
+                | "e"  -> pos <- { x = pos.x + 1; y = pos.y + 0; }
+                | "se" -> pos <- { x = pos.x + 1; y = pos.y - 1; }
+                | "sw" -> pos <- { x = pos.x + 0; y = pos.y - 1; }
+                | "w"  -> pos <- { x = pos.x - 1; y = pos.y + 0; }
+                | "nw" -> pos <- { x = pos.x - 1; y = pos.y + 1; }
+                | "ne" -> pos <- { x = pos.x + 0; y = pos.y + 1; }
+                | _ -> failwith "not implemented"
+        if tiles.ContainsKey(pos) then
+            tiles.[pos] <- if tiles.[pos] = 'w' then 'b' else 'w'
+        else
+            tiles.[pos] <- 'b'
+
+    (tiles.Values |> Seq.filter (fun x -> x = 'b') |> Seq.length)
