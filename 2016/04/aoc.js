9673d03885cab791053fba8db64c52f117e55037
Dirk Lemstra <dirk@lemstra.org>
2021-11-09 00:10:36 +0100

    Implemented day 4 part 2.

--- 2016/04/aoc.js
+++ 2016/04/aoc.js
@@ -17,23 +17,46 @@ function sortByFrequency(array) {
     });
 }
 
+function decode(encoded, count) {
+    const input  = 'abcdefghijklmnopqrstuvwxyz'
+    const output = 'bcdefghijklmnopqrstuvwxyza'
+
+    let decoded = encoded.split('')
+
+    while (count-- > 0) {
+        for (let i=0; i < decoded.length; i++) {
+            const index = input.indexOf(decoded[i])
+            decoded[i] = output[index]
+        }
+    }
+
+    return decoded.join('');
+}
+
 module.exports = {
     securityThroughObscurity: function(input) {
         let sum = 0
+        let northPoleObjectsSectorId = 0
 
         input.split('\n').forEach((line) => {
-            let chars = line.trim().split('-')
-            const last = chars.pop().split('[')
-            chars = chars.join('').split('')
+            const words = line.trim().split('-')
+            const last = words.pop().split('[')
+            let chars = words.join('').split('')
             chars = sortByFrequency(chars)
 
             const name = last[1].slice(0, -1)
+            const sectorId = parseInt(last[0])
             if (name == chars.slice(0, 5).join('')) {
-                const sectorId = parseInt(last[0])
                 sum += sectorId
+            }
+
+            let count = sectorId % 26
+            const roomName = `${decode(words[0], count)} ${decode(words[1], count)} ${decode(words[2], count)}`
+            if (roomName == 'northpole object storage') {
+                northPoleObjectsSectorId = sectorId
             }
         })
 
-        return sum
+        return [sum, northPoleObjectsSectorId]
     }
 }
--- 2016/04/test.js
+++ 2016/04/test.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const aoc = require('./aoc')
 
-let part1 = aoc.securityThroughObscurity(`aaaaa-bbb-z-y-x-123[abxyz]
+let [part1, part2] = aoc.securityThroughObscurity(`aaaaa-bbb-z-y-x-123[abxyz]
 a-b-c-d-e-f-g-h-987[abcde]
 not-a-real-room-404[oarel]
 totally-real-room-200[decoy]`)
