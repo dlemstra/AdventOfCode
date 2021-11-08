62cc0cdcbdc2a3b19067975c72b3d479c4f5f687
Dirk Lemstra <dirk@lemstra.org>
2021-11-08 23:02:19 +0100

    Implemented day 3 part 2

--- 2016/03/aoc.js
+++ 2016/03/aoc.js
@@ -1,13 +1,28 @@
+function checkNumbers(a, b, c) {
+    return a + b > c &&
+        a + c > b &&
+        b + c > a
+}
+
 module.exports = {
     squaresWithThreeSides: function(input) {
-        let correct = 0
+        let correct1 = 0
+        let correct2 = 0
+        let rows = []
         input.split('\n').forEach((line) => {
             const numbers = line.split(' ').filter(n => n).map(n => parseInt(n))
-            if (numbers[0] + numbers[1] > numbers[2] &&
-                numbers[0] + numbers[2] > numbers[1] &&
-                numbers[1] + numbers[2] > numbers[0]) { correct++ }
+            if (checkNumbers(numbers[0], numbers[1], numbers[2])) { correct1++ }
+
+            rows.push(numbers)
+            if (rows.length == 3) {
+                if (checkNumbers(rows[0][0], rows[1][0], rows[2][0])) { correct2++ }
+                if (checkNumbers(rows[0][1], rows[1][1], rows[2][1])) { correct2++ }
+                if (checkNumbers(rows[0][2], rows[1][2], rows[2][2])) { correct2++ }
+
+                rows = []
+            }
         })
 
-        return correct
+        return [correct1, correct2]
     }
 }
--- 2016/03/test.js
+++ 2016/03/test.js
@@ -1,5 +1,6 @@
 const assert = require('assert')
 const aoc = require('./aoc')
 
-let part1 = aoc.squaresWithThreeSides('5 10 25\n16 10 25')
-assert.equal(part1, 1)
+let [part1, part2] = aoc.squaresWithThreeSides('5 10 25\n7 11 16\n3 14 15')
+assert.equal(part1, 2)
+assert.equal(part2, 3)
