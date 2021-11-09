2c5bbcd6f5f1bc5b47617098a342600e212ced2d
Dirk Lemstra <dirk@lemstra.org>
2021-11-09 18:15:09 +0100

    Implemented day 7 part 2.

--- 2016/07/aoc.js
+++ 2016/07/aoc.js
@@ -1,7 +1,7 @@
-function isValid(line) {
+function supportTLS(line) {
     let valid = 0
     let insideBrackets = false
-    for (let i=0; i < line.length-4; i++) {
+    for (let i=0; i < line.length-3; i++) {
         if (line[i] == '[') { insideBrackets = true }
         if (line[i] == ']') { insideBrackets = false }
 
@@ -17,10 +17,43 @@ function isValid(line) {
     return valid
 }
 
+function supportSSL(line) {
+    let found_inside = []
+    let found_outside = []
+
+    let insideBrackets = false
+    for (let i=0; i < line.length-2; i++) {
+        if (line[i] == '[') { insideBrackets = true }
+        if (line[i] == ']') { insideBrackets = false }
+
+        if (line[i + 0] != line[i + 1] &&
+            line[i + 1] != '[' &&
+            line[i + 1] != ']' &&
+            line[i + 0] == line[i + 2])
+        {
+            const value = line.slice(i, i + 3)
+            if (insideBrackets) { found_inside.push(value) }
+            else { found_outside.push(value) }
+        }
+    }
+
+    let valid = 0
+    found_inside.forEach(value => {
+        const expected = `${value[1]}${value[0]}${value[1]}`
+        if (found_outside.includes(expected)) { valid = 1 }
+    });
+
+    return valid
+}
+
 module.exports = {
     internetProtocolVersion7: function(input) {
-        const items = input.split('\n').map(line => isValid(line));
+        const items1 = input.split('\n').map(line => supportTLS(line));
+        const items2 = input.split('\n').map(line => supportSSL(line));
 
-        return items.reduce((partial_sum, value) => partial_sum + value, 0);
+        const part1 = items1.reduce((partial_sum, value) => partial_sum + value, 0);
+        const part2 = items2.reduce((partial_sum, value) => partial_sum + value, 0);
+
+        return [ part1, part2 ]
     }
 }
--- 2016/07/test.js
+++ 2016/07/test.js
@@ -1,8 +1,16 @@
 const assert = require('assert')
 const aoc = require('./aoc')
 
-let part1 = aoc.internetProtocolVersion7(`abba[mnop]qrst
+let [part1_1, part2_1] = aoc.internetProtocolVersion7(`abba[mnop]qrst
 abcd[bddb]xyyx
 aaaa[qwer]tyui
 ioxxoj[asdfgh]zxcvbn`)
-assert.equal(part1, 2)
+assert.equal(part1_1, 2)
+assert.equal(part2_1, 0)
+
+let [part1_2, part2_2] = aoc.internetProtocolVersion7(`aba[bab]xyz
+xyx[xyx]xyx
+aaa[kek]eke
+zazbz[bzb]cdb`)
+assert.equal(part1_2, 0)
+assert.equal(part2_2, 3)
