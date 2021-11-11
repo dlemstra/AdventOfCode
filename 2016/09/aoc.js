980a7fa1d4271b8891c6e35c662b31349c4df2e5
Dirk Lemstra <dirk@lemstra.org>
2021-11-11 18:30:06 +0100

    Implemented day 9 part 2.

--- 2016/09/aoc.js
+++ 2016/09/aoc.js
@@ -1,7 +1,8 @@
-function calulateLength(input) {
-    let length = 0
+function calulateLength(input, start_index, length) {
+    let part1 = 0
+    let part2 = 0
     let start = -1
-    for (let i=0; i < input.length; i++) {
+    for (let i=start_index; i < length; i++) {
         switch(input[i]) {
             case '(':
                 start = i
@@ -12,22 +13,38 @@ function calulateLength(input) {
                 const times = parseInt(info[1])
                 const end = Math.min(i + 1 + count, input.length)
                 if (i + 1 != end) {
-                    length += times * (end - i - 1)
+                    part1 += times * (end - i - 1)
+
+                    for (let j=0; j < times; j++) {
+                        let [_, child_part2] =  calulateLength(input, i + 1, end)
+                        part2 += child_part2
+                    }
                 }
                 i += count
                 start = -1
                 break;
             default:
-                if (start == -1) { length++ }
+                if (start == -1) {
+                    part1++
+                    part2++
+                }
                 break
         }
     }
 
-    return length
+    return [part1, part2]
 }
 
 module.exports = {
     explosivesInCyberspace: function(input) {
-        return input.split('\n').map(line => calulateLength(line)).reduce((partial_sum, value) => partial_sum + value, 0)
+        let total_part1 = 0;
+        let total_part2 = 0;
+        input.split('\n').forEach(line => {
+            let [part1, part2] = calulateLength(line, 0, line.length)
+            total_part1 += part1
+            total_part2 += part2
+        });
+
+        return [total_part1, total_part2]
     }
 }
--- 2016/09/test.js
+++ 2016/09/test.js
@@ -1,10 +1,17 @@
 const assert = require('assert')
 const aoc = require('./aoc')
 
-let part1 = aoc.explosivesInCyberspace(`ADVENT
+let [part1, a] = aoc.explosivesInCyberspace(`ADVENT
 A(1x5)BC
 (3x3)XYZ
 (6x1)(1x3)A
 X(8x2)(3x3)ABCY`)
 
 assert.equal(part1, 46)
+
+let [b, part2] = aoc.explosivesInCyberspace(`(3x3)XYZ
+X(8x2)(3x3)ABCY
+(27x12)(20x12)(13x14)(7x10)(1x12)A
+(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN`)
+
+assert.equal(part2, 242394)
