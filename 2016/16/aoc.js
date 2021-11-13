dd35d0de3faae49aa3bebc6de1e46c6a735f77ed
Dirk Lemstra <dirk@lemstra.org>
2021-11-13 12:02:55 +0100

    Implemented day 16 part 2.

--- 2016/16/aoc.js
+++ 2016/16/aoc.js
@@ -8,19 +8,27 @@ function createChecksum(data) {
     return checksum
 }
 
+function getCheckSum(start, length) {
+    let a = [...start]
+    while (a.length < length) {
+        const b = [...a].reverse().map(x => (x + 1) % 2)
+        a = a.concat([0]).concat(b)
+    }
+
+    let c = createChecksum(a.slice(0, length))
+    while (c.length % 2 == 0) {
+        c = createChecksum(c)
+    }
+
+    return c.join('')
+}
+
 module.exports = {
-    dragonChecksum: function(input, length) {
-        let a = input.split('').map(c => parseInt(c))
-        while (a.length < length) {
-            const b = [...a].reverse().map(x => (x + 1) % 2)
-            a = a.concat([0]).concat(b)
-        }
+    dragonChecksum: function(input, length1, length2) {
+        const start = input.split('').map(c => parseInt(c))
+        let part1 = getCheckSum(start, length1)
+        let part2 = getCheckSum(start, length2)
 
-        let c = createChecksum(a.slice(0, length))
-        while (c.length % 2 == 0) {
-            c = createChecksum(c)
-        }
-
-        return c.join('')
+        return [part1, part2]
     }
 }
--- 2016/16/test.js
+++ 2016/16/test.js
@@ -1,10 +1,12 @@
 const assert = require('assert')
 const aoc = require('./aoc')
 
-let part1 = aoc.dragonChecksum('110010110100', 12)
+let [part1_1, part2_1] = aoc.dragonChecksum('110010110100', 12, 12)
 
-assert.equal(part1, '100')
+assert.equal(part1_1, '100')
+assert.equal(part2_1, '100')
 
-part1 = aoc.dragonChecksum('10000', 20)
+let [part1_2, part2_2] = aoc.dragonChecksum('10000', 20, 20)
 
-assert.equal(part1, '01100')
+assert.equal(part1_2, '01100')
+assert.equal(part2_2, '01100')
