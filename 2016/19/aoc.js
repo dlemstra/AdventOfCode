0923f48f4231a1e9d691ef6ad342f37b8e6c6788
Dirk Lemstra <dirk@lemstra.org>
2021-11-13 23:07:23 +0100

    Implemented day 19 part 2.

--- 2016/19/aoc.js
+++ 2016/19/aoc.js
@@ -1,25 +1,62 @@
+function LinkedList() {
+    this.Node = null;
+    this.head = null;
+};
+
+LinkedList.prototype.append = function(value) {
+    var node = {
+        value: value,
+        next: this.head
+    };
+
+    if (this.head === null) {
+        this.head = {};
+        this.head.value = value;
+        this.head.next = this.head;
+        this.tail = this.head;
+    } else {
+        this.tail.next = node;
+        this.tail = node;
+    }
+
+    return node
+};
+
 module.exports = {
     anElephantNamedJoseph: function(input) {
         const count = parseInt(input)
-        const circle = Array(count).fill(1)
 
-        let index = 0
-        while (true) {
-            let next = (index + 1) % count
-            while (circle[next] == 0 ) {
-                next = (next + 1) % count
-            }
-
-            if (next == index) { return index + 1 }
-            circle[index] += circle[next]
-            circle[next] = 0
-
-            index = (index + 1) % count
-            while (circle[index] == 0 ) {
-                index = (index + 1) % count
-            }
+        let linkedList = new LinkedList()
+        for (let i=1; i <= count; i++) {
+            linkedList.append(i)
         }
 
-        return -1
+        let node = linkedList.head
+        while (node.next != node) {
+            node.next = node.next.next
+            node = node.next
+        }
+
+        const part1 = node.value
+
+        let mid = null
+        let midIndex = Math.floor(count / 2)
+        linkedList = new LinkedList()
+        for (let i=1; i <= count; i++) {
+            const node = linkedList.append(i)
+            if (i == midIndex) { mid = node }
+        }
+
+        node = linkedList.head
+        let remaining = count
+        while (node.next != node) {
+            mid.next = mid.next.next
+            if (--remaining % 2 == 0) { mid = mid.next }
+            node = node.next
+        }
+
+        const part2 = node.value
+
+        return [part1, part2]
     }
 }
--- 2016/19/test.js
+++ 2016/19/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert')
 const aoc = require('./aoc')
 
-let part1 = aoc.anElephantNamedJoseph('5')
+let [part1, part2] = aoc.anElephantNamedJoseph('5')
 
 assert.equal(part1, 3)
+assert.equal(part2, 2)
