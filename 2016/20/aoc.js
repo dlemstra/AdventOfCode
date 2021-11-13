52e4a3fe999cb03620063dc910103e9a101d8063
Dirk Lemstra <dirk@lemstra.org>
2021-11-13 23:31:45 +0100

    Implemented day 20 part 2.

--- 2016/20/aoc.js
+++ 2016/20/aoc.js
@@ -6,15 +6,23 @@ module.exports = {
             ranges.push([parseInt(info[0]), parseInt(info[1])])
         })
 
+        let part1 = -1
+        let part2 = 0
+
         let value = 0
         let range = [1]
 
-        while (true) {
+        while (value < 4294967295) {
             range = ranges.filter(range => value >= range[0] && value <= range[1])
-            if (range.length == 0) { break}
-            value = range[0][1] + 1
+            if (range.length == 0) {
+                if (part1 == -1) { part1 = value }
+                part2++
+                value = value + 1
+            } else {
+                value = range[0][1] + 1
+            }
         }
 
-        return value
+        return [part1, part2]
     }
 }
