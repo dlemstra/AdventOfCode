40195466c10505889ba1a143965f77d80cc39745
Dirk Lemstra <dirk@lemstra.org>
2021-11-13 23:33:19 +0100

    Corrected output.

--- 2016/20/main.js
+++ 2016/20/main.js
@@ -3,5 +3,6 @@ const aoc = require('./aoc')
 
 const input = fs.readFileSync('input', 'utf8')
 
-let part1 = aoc.firewallRules(input)
+let [part1, part2] = aoc.firewallRules(input)
 console.log('Part 1:', part1)
+console.log('Part 2:', part2)
