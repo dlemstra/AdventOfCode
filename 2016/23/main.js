bdea455f5fc153186e56e97040019b35dcacf2df
Dirk Lemstra <dirk@lemstra.org>
2021-11-17 08:31:52 +0100

    Implemented day 23 part 2.

--- 2016/23/main.js
+++ 2016/23/main.js
@@ -5,3 +5,7 @@ const input = fs.readFileSync('input', 'utf8')
 
 let part1 = aoc.safeCracking(input, 7)
 console.log('Part 1:', part1)
+
+// Completes in 4 minutes but still faster than optimizing it
+let part2 = aoc.safeCracking(input, 12)
+console.log('Part 2:', part2)
