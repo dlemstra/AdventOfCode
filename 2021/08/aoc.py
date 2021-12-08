c21d211d653564544492beb75df388340430d0af
Dirk Lemstra <dirk@lemstra.org>
2021-12-08 18:13:29 +0100

    Implemented day 8 part 2.

--- 2021/08/aoc.py
+++ 2021/08/aoc.py
@@ -1,5 +1,3 @@
-import sys
-
 def matches1478(value):
     count = 0
     for v in value.split(' '):
@@ -8,11 +6,76 @@ def matches1478(value):
             count += 1
     return count
 
+def findByLength(numbers, length):
+    return (number for number in numbers if len(number) == length)
+
+def getNumberMapping(value):
+    info = value.split(' ')
+    one = list(next(findByLength(info, 2)))
+    four = list(next(findByLength(info, 4)))
+    seven = list(next(findByLength(info, 3)))
+    eight = list(next(findByLength(info, 7)))
+
+    sixCount = list(findByLength(info, 6))
+    for value in sixCount:
+        remaining = [x for x in value if x not in four]
+        remaining = [x for x in remaining if x not in seven]
+        if len(remaining) == 1:
+            nine = list(value)
+
+    for value in [x for x in sixCount if x != ''.join(nine)]:
+        remaining = [x for x in value if x not in seven]
+        if len(remaining) == 3:
+            zero = list(value)
+        else:
+            six = list(value)
+
+    fiveCount = list(findByLength(info, 5))
+    for value in fiveCount:
+        remaining = [x for x in value if x not in seven]
+        if len(remaining) == 2:
+            three = list(value)
+
+    for value in [x for x in fiveCount if x != ''.join(three)]:
+        remaining = [x for x in value if x not in six]
+        if len(remaining) == 0:
+            five = list(value)
+        else:
+            two = list(value)
+
+    mapping = {}
+    mapping[''.join(sorted(zero))] = '0'
+    mapping[''.join(sorted(one))] = '1'
+    mapping[''.join(sorted(two))] = '2'
+    mapping[''.join(sorted(three))] = '3'
+    mapping[''.join(sorted(four))] = '4'
+    mapping[''.join(sorted(five))] = '5'
+    mapping[''.join(sorted(six))] = '6'
+    mapping[''.join(sorted(seven))] = '7'
+    mapping[''.join(sorted(eight))] = '8'
+    mapping[''.join(sorted(nine))] = '9'
+    return mapping
+
+def getOutput(line):
+    (start, end) = line.split(' | ')
+
+    mapping = getNumberMapping(start)
+
+    code = ''
+    for value in end.split(' '):
+        value = ''.join(sorted(list(value)))
+        code += mapping[value]
+
+    return int(code)
+
 def sevenSegmentSearch(input):
     part1 = 0
-
     for line in input:
-        (start, end) = line.split(' | ')
+        (_, end) = line.split(' | ')
         part1 += matches1478(end)
 
-    return (part1, None)
+    part2 = 0
+    for line in input:
+        part2 += getOutput(line)
+
+    return (part1, part2)
--- 2021/08/test.py
+++ 2021/08/test.py
@@ -15,5 +15,6 @@ bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbg
 egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce""".splitlines())
         self.assertEqual(part1, 26)
+        self.assertEqual(part2, 61229)
 
 unittest.main()
