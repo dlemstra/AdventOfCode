cd6501969ebc853d1aa0833dd494d9de90a106b3
Dirk Lemstra <dirk@lemstra.org>
2021-12-16 18:07:27 +0100

    Implemented day 16 part 2.

--- 2021/16/aoc.py
+++ 2021/16/aoc.py
@@ -1,3 +1,11 @@
+def parseInt(buffer):
+    value = 0
+    for bit in buffer:
+        value <<= 1
+        value |= bit
+
+    return value
+
 class BitReader:
     def __init__(self, data, buffer = []):
         self.data = data
@@ -7,16 +15,8 @@ class BitReader:
         return (len(self.data) * 4) + len(self.buffer) < 8
 
     def readBits(self, count):
-        value = 0
-        read = 0
-        while count != read:
-            value = value << 1
-            self.fillBuffer()
-            bit = self.buffer.pop(0)
-            value = value | bit
-            read += 1
-
-        return value
+        buffer = self.readBuffer(count)
+        return parseInt(buffer)
 
     def readBuffer(self, count):
         buffer = []
@@ -46,27 +46,52 @@ def parseBuffer(reader):
     part1 = version
 
     if type == 4:
-        group = reader.readBits(5)
-        while group >> 4 != 0:
-            group = reader.readBits(5)
-
-        return part1
+        buffer = []
+        bit = 1
+        while bit != 0:
+            group = reader.readBuffer(5)
+            bit = group.pop(0)
+            buffer.extend(group)
+        return (part1, parseInt(buffer))
     else:
+        values = []
         if reader.readBits(1):
             count = reader.readBits(11)
             for _ in range(0, count):
-                part1 += parseBuffer(reader)
+                (sub_part1, sub_part2) = parseBuffer(reader)
+                part1 += sub_part1
+                values.append(sub_part2)
         else:
             length = reader.readBits(15)
             data = reader.readBuffer(length)
             subReader = BitReader([], data)
             while not subReader.done():
-                part1 += parseBuffer(subReader)
+                (sub_part1, sub_part2) = parseBuffer(subReader)
+                part1 += sub_part1
+                values.append(sub_part2)
 
-        return part1
+        match type:
+            case 0:
+                part2 = sum(values)
+            case 1:
+                part2 = 1
+                for value in values:
+                    part2 *= value
+            case 2:
+                part2 = min(values)
+            case 3:
+                part2 = max(values)
+            case 4:
+                part2 = 0
+            case 5:
+                part2 = 1 if values[0] > values[1] else 0
+            case 6:
+                part2 = 1 if values[0] < values[1] else 0
+            case 7:
+                part2 = 1 if values[0] == values[1] else 0
+
+        return (part1, part2)
 
 def packetDecoder(input):
     reader = BitReader(list(input[0]))
-    part1 = parseBuffer(reader)
-
-    return (part1, None)
+    return parseBuffer(reader)
--- 2021/16/test.py
+++ 2021/16/test.py
@@ -4,19 +4,52 @@ from aoc import packetDecoder
 class TestAoc(unittest.TestCase):
 
     def test(self):
-        (part1, part2) = packetDecoder("8A004A801A8002F478".splitlines())
+        (part1, _) = packetDecoder("8A004A801A8002F478".splitlines())
         self.assertEqual(part1, 16)
 
     def test2(self):
-        (part1, part2) = packetDecoder("620080001611562C8802118E34".splitlines())
+        (part1, _) = packetDecoder("620080001611562C8802118E34".splitlines())
         self.assertEqual(part1, 12)
 
     def test3(self):
-        (part1, part2) = packetDecoder("C0015000016115A2E0802F182340".splitlines())
+        (part1, _) = packetDecoder("C0015000016115A2E0802F182340".splitlines())
         self.assertEqual(part1, 23)
 
     def test4(self):
-        (part1, part2) = packetDecoder("A0016C880162017C3686B18A3D4780".splitlines())
+        (part1, _) = packetDecoder("A0016C880162017C3686B18A3D4780".splitlines())
         self.assertEqual(part1, 31)
+
+    def test5(self):
+        (_, part2) = packetDecoder("C200B40A82".splitlines())
+        self.assertEqual(part2, 3)
+
+    def test6(self):
+        (_, part2) = packetDecoder("04005AC33890".splitlines())
+        self.assertEqual(part2, 54)
+
+    def test7(self):
+        (_, part2) = packetDecoder("880086C3E88112".splitlines())
+        self.assertEqual(part2, 7)
+
+    def test8(self):
+        (_, part2) = packetDecoder("CE00C43D881120".splitlines())
+        self.assertEqual(part2, 9)
+
+    def test8(self):
+        (_, part2) = packetDecoder("D8005AC2A8F0".splitlines())
+        self.assertEqual(part2, 1)
+
+    def test9(self):
+        (_, part2) = packetDecoder("F600BC2D8F".splitlines())
+        self.assertEqual(part2, 0)
+
+    def test10(self):
+        (_, part2) = packetDecoder("9C005AC2F8F0".splitlines())
+        self.assertEqual(part2, 0)
+
+    def test11(self):
+        (_, part2) = packetDecoder("9C0141080250320F1802104A08".splitlines())
+        self.assertEqual(part2, 1)
+
 
 unittest.main()
