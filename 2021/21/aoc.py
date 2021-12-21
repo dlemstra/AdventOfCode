29ad8eb7fb2e3b54bca7d63154f548e19c741448
Dirk Lemstra <dirk@lemstra.org>
2021-12-21 11:34:36 +0100

    Implemented day 21 part 2.

--- 2021/21/aoc.py
+++ 2021/21/aoc.py
@@ -1,40 +1,76 @@
-def roll(dice):
-    total = dice
-    dice += 1
-    total += dice
-    dice += 1
-    total += dice
-    dice += 1
-    return (dice, total)
-
-def diracDice(input):
-    part1 = 0
-
-    player1_pos = int(input[0].split(': ')[1])
+def dicePart1(player1_pos, player2_pos):
     player1_score = 0
-    player2_pos = int(input[1].split(': ')[1])
     player2_score = 0
 
     dice = 1
     while player1_score < 1000 and player2_score < 1000:
-        (dice, total) = roll(dice)
+        total = dice + dice + 1 + dice + 2
         player1_pos = (player1_pos + total) % 10
         if player1_pos == 0:
             player1_pos = 10
         player1_score += player1_pos
+        dice += 3
         if player1_score >= 1000:
             break
 
-        (dice, total) = roll(dice)
+        total = dice + dice + 1 + dice + 2
         player2_pos = (player2_pos + total) % 10
         if player2_pos == 0:
             player2_pos = 10
         player2_score += player2_pos
+        dice += 3
 
     dice -= 1
     if player1_score > player2_score:
-        part1 = player2_score * dice
+        return player2_score * dice
     else:
-        part1 = player1_score * dice
+        return player1_score * dice
 
-    return (part1, None)
+def dicePart2(player1_pos, player1_score, player2_pos, player2_score, player, dice, totals, total):
+    if dice > 0:
+        if player == 1:
+            player1_pos = (player1_pos + dice) % 10
+            if player1_pos == 0:
+                player1_pos = 10
+            player1_score += player1_pos
+            if player1_score >= total:
+                return (1, 0)
+        else:
+            player2_pos = (player2_pos + dice) % 10
+            if player2_pos == 0:
+                player2_pos = 10
+            player2_score += player2_pos
+            if player2_score >= total:
+                return (0, 1)
+
+    one_wins = 0
+    two_wins = 0
+
+    next_player = 2 if player == 1 else 1
+    for dice in totals:
+        (one, two) = dicePart2(player1_pos, player1_score, player2_pos, player2_score, next_player, dice, totals, total)
+        if one > 0:
+            one_wins += one * totals[dice]
+        if two > 0:
+            two_wins += two * totals[dice]
+
+    return (one_wins, two_wins)
+
+def diracDice(input):
+    player1_pos = int(input[0].split(': ')[1])
+    player2_pos = int(input[1].split(': ')[1])
+    part1 = dicePart1(player1_pos, player2_pos)
+
+    totals = {}
+    for one in range(1, 4):
+        for two in range(1, 4):
+            for three in range(1, 4):
+                total = one + two + three
+                if total not in totals:
+                    totals[total] = 0
+                totals[total] += 1
+
+    (one, two) = dicePart2(player1_pos, 0, player2_pos, 0, 2, 0, totals, 21)
+    part2 = max(one, two)
+
+    return (part1, part2)
--- 2021/21/test.py
+++ 2021/21/test.py
@@ -7,5 +7,6 @@ class TestAoc(unittest.TestCase):
         (part1, part2) = diracDice("""Player 1 starting position: 4
 Player 2 starting position: 8""".splitlines())
         self.assertEqual(part1, 739785)
+        self.assertEqual(part2, 444356092776315)
 
 unittest.main()
