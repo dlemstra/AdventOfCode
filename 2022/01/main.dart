2581bd853f53abfdddec6e7d08618acbe228a18f
Dirk Lemstra <dirk@lemstra.org>
2022-12-01 15:29:53 +0100

    Implemented day 1 part 2.

--- 2022/01/main.dart
+++ 2022/01/main.dart
@@ -1,23 +1,39 @@
 import 'dart:io';
-import 'dart:math';
 
-void part1(List<String> lines) {
-  var highest = 0;
+List<int> getTotals(List<String> lines) {
+  final result = <int>[];
+
   var total = 0;
-  for (var line in lines) {
+  for (final line in lines) {
     if (line == '') {
-      highest = max(highest, total);
+      result.add(total);
       total = 0;
     } else {
       total += int.parse(line);
     }
   }
-  print(highest);
+
+  if (total != 0) {
+      result.add(total);
+  }
+
+  result.sort((b, a) => a.compareTo(b));
+  return result;
+}
+
+int part1(List<int> totals) {
+  return totals[0];
+}
+
+int part2(List<int> totals) {
+  return totals.take(3).fold(0, (previous, current) => previous + current);
 }
 
 void main() {
-  var input = new File("input");
-  var lines = input.readAsLinesSync();
+  final input = new File("input");
+  final lines = input.readAsLinesSync();
 
-  part1(lines);
+  final totals = getTotals(lines);
+  print(part1(totals));
+  print(part2(totals));
 }
