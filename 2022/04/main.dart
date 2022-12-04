e90af8c72a60ec02d1fe3db8a15185675362ab2c
Dirk Lemstra <dirk@lemstra.org>
2022-12-04 08:40:49 +0100

    Implemented day 4 part 2.

--- 2022/04/main.dart
+++ 2022/04/main.dart
@@ -1,23 +1,31 @@
 import 'dart:io';
+import 'dart:math';
 
-int part1(List<String> jobs) {
-  var count = 0;
+void solve(List<String> jobs) {
+  var part1 = 0;
+  var part2 = 0;
 
   for (final job in jobs) {
     final info = job.split(",");
     final a = info[0].split("-").map((e) => int.parse(e)).toList();
     final b = info[1].split("-").map((e) => int.parse(e)).toList();
     if ((a[0] >= b[0] && a[1] <= b[1]) || (b[0] >= a[0] && b[1] <= a[1])) {
-      count += 1;
+      part1++;
+    }
+    final s = max(a[0], b[0]);
+    final e = min(a[1], b[1]);
+    if (s <= e) {
+      part2++;
     }
   }
 
-  return count;
+  print(part1);
+  print(part2);
 }
 
 void main() {
   final input = new File("input");
   final jobs = input.readAsLinesSync();
 
-  print(part1(jobs));
+  solve(jobs);
 }
