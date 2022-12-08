887bdd8e10f08349e8c7fa5203638a0057393b45
Dirk Lemstra <dirk@lemstra.org>
2022-12-08 08:33:57 +0100

    Implemented day 8 part 2.

--- 2022/08/main.dart
+++ 2022/08/main.dart
@@ -42,8 +42,47 @@ bool isVisible(Map map, int size, int x, int y, Direction direction) {
   return true;
 }
 
+int visibleCount(Map map, int max, int x, int y, Direction direction) {
+  final value = map["${x}x${y}"];
+  var count = 0;
+  switch (direction) {
+    case Direction.up:
+      while (y > 0) {
+        count++;
+        if (value <= map["${x}x${--y}"]) {
+          return count;
+        }
+      }
+      break;
+    case Direction.down:
+      while (y < max) {
+        count++;
+        if (value <= map["${x}x${++y}"]) {
+          return count;
+        }
+      }
+      break;
+    case Direction.left:
+      while (x > 0) {
+        count++;
+        if (value <= map["${--x}x${y}"]) {
+          return count;
+        }
+      }
+      break;
+    case Direction.right:
+      while (x < max) {
+        count++;
+        if (value <= map["${++x}x${y}"]) {
+          return count;
+        }
+      }
+      break;
+  }
+  return count;
+}
 
-void part1(List<String> lines) {
+void solve(List<String> lines) {
   final map = new Map();
 
   var y = 0;
@@ -57,25 +96,41 @@ void part1(List<String> lines) {
   }
 
   final size = y;
-  var visible = (size * 4) - 4;
+  final max = size - 1;
 
+  var part1 = (size * 4) - 4;
   for (var y = 1; y < size - 1; y++) {
     for (var x = 1; x < size - 1; x++) {
       if (isVisible(map, size, x, y, Direction.up) ||
           isVisible(map, size, x, y, Direction.down) ||
           isVisible(map, size, x, y, Direction.left) ||
           isVisible(map, size, x, y, Direction.right)) {
-        visible++;
+        part1++;
       }
     }
   }
 
-  print(visible);
+  print(part1);
+
+  var part2 = 0;
+  for (var y = 1; y < size - 1; y++) {
+    for (var x = 1; x < size - 1; x++) {
+      final score = visibleCount(map, max, x, y, Direction.up) *
+                    visibleCount(map, max, x, y, Direction.down) *
+                    visibleCount(map, max, x, y, Direction.left) *
+                    visibleCount(map, max, x, y, Direction.right);
+      if (score > part2) {
+        part2 = score;
+      }
+    }
+  }
+
+  print(part2);
 }
 
 void main() {
-  final input = new File("testinput");
+  final input = new File("input");
   final lines = input.readAsLinesSync();
 
-  part1(lines);
+  solve(lines);
 }
