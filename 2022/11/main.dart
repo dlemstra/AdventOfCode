18bc00d7d743a0654b882c4cf445684f89772e55
Dirk Lemstra <dirk@lemstra.org>
2022-12-11 13:15:22 +0100

    Implemented day 11 part 2.

--- 2022/11/main.dart
+++ 2022/11/main.dart
@@ -1,69 +1,72 @@
 import 'dart:io';
-import 'dart:math';
 
 class Monkey {
-  List<int> items = [];
-  bool multiplication = false;
-  int value = 0;
-  int diviser = 0;
-  int trueTarget = 0;
-  int falseTarget = 0;
-  int inspected = 0;
+    List<int> items = [];
+    bool multiplication = false;
+    int value = 0;
+    int diviser = 0;
+    int trueTarget = 0;
+    int falseTarget = 0;
+    int inspected = 0;
 
-  int getNextValue() {
-    var result = this.items.removeAt(0);
-    final value = this.value == -1 ? result : this.value;
-    if (this.multiplication) {
-        result *= value;
-    } else {
-        result += value;
+    int getNextValue() {
+        this.inspected++;
+
+        final result = this.items.removeAt(0);
+        final value = this.value == -1 ? result : this.value;
+        if (this.multiplication) {
+            return result * value;
+        }
+
+        return result + value;
     }
-
-    return (result / 3).floor();
-  }
-
-  @override
-  String toString() {
-    return "${this.items} ${this.multiplication} ${this.value} ${this.diviser} ${this.trueTarget} ${this.falseTarget} ${this.inspected}";
-  }
 }
 
 List<Monkey> loadMonkeys(List<String> lines) {
-  final monkeys = <Monkey>[];
-  monkeys.add(new Monkey());
+    final monkeys = <Monkey>[];
+    monkeys.add(new Monkey());
 
-  for (var line in lines) {
-    if (line.startsWith("  Starting")) {
-      monkeys.last.items = line.split(":")[1].split(",").map((v) => int.parse(v)).toList();
-    } else if (line.startsWith("  Operation:")) {
-      var info = line.split(" ");
-      monkeys.last.multiplication = info[6] == "*";
-      if (info[7] == "old") {
-        monkeys.last.value = -1;
-      } else {
-        monkeys.last.value = int.parse(info[7]);
-      }
-    } else if (line.startsWith("  Test:")) {
-        monkeys.last.diviser = int.parse(line.split(" ").last);
-    } else if (line.startsWith("    If true:")) {
-        monkeys.last.trueTarget = int.parse(line.split(" ").last);
-    } else if (line.startsWith("    If false:")) {
-        monkeys.last.falseTarget = int.parse(line.split(" ").last);
-    } else if (line == "") {
-        monkeys.add(new Monkey());
+    for (var line in lines) {
+        if (line.startsWith("  Starting")) {
+            monkeys.last.items = line.split(":")[1].split(",").map((v) => int.parse(v)).toList();
+        } else if (line.startsWith("  Operation:")) {
+            var info = line.split(" ");
+            monkeys.last.multiplication = info[6] == "*";
+            if (info[7] == "old") {
+                monkeys.last.value = -1;
+            } else {
+                monkeys.last.value = int.parse(info[7]);
+            }
+        } else if (line.startsWith("  Test:")) {
+            monkeys.last.diviser = int.parse(line.split(" ").last);
+        } else if (line.startsWith("    If true:")) {
+            monkeys.last.trueTarget = int.parse(line.split(" ").last);
+        } else if (line.startsWith("    If false:")) {
+            monkeys.last.falseTarget = int.parse(line.split(" ").last);
+        } else if (line == "") {
+            monkeys.add(new Monkey());
+        }
     }
-  }
 
   return monkeys;
 }
 
-int solve(List<Monkey> monkeys, int rounds) {
+void solve(List<String> lines, bool divideByThree, int rounds) {
+    final monkeys = loadMonkeys(lines);
+    final diviserProduct = monkeys.fold(1, (acc, monkey) => acc * monkey.diviser);
+
     while (rounds-- != 0) {
         for (final monkey in monkeys) {
             while (monkey.items.length > 0) {
-                final nextValue = monkey.getNextValue();
-                monkeys[nextValue % monkey.diviser == 0 ? monkey.trueTarget : monkey.falseTarget].items.add(nextValue);
-                monkey.inspected++;
+                var nextValue = monkey.getNextValue();
+                if (divideByThree) {
+                    nextValue = nextValue ~/ 3;
+                } else {
+                    nextValue %= diviserProduct;
+                }
+
+                final target = nextValue % monkey.diviser == 0 ? monkey.trueTarget : monkey.falseTarget;
+                monkeys[target].items.add(nextValue);
             }
         }
     }
@@ -71,14 +74,13 @@ int solve(List<Monkey> monkeys, int rounds) {
     var inspectCounts = monkeys.map((m) => m.inspected).toList();
     inspectCounts.sort();
 
-    return inspectCounts.removeLast() * inspectCounts.removeLast();
+    print(inspectCounts.removeLast() * inspectCounts.removeLast());
 }
 
 void main() {
-  final input = new File("input");
-  final lines = input.readAsLinesSync();
+    final input = new File("input");
+    final lines = input.readAsLinesSync();
 
-  final monkeys = loadMonkeys(lines);
-  print(solve(monkeys, 20));
-  print(solve(monkeys, 10000));
+    solve(lines, true, 20);
+    solve(lines, false, 10000);
 }
