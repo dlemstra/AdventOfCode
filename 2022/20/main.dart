5f7991d1e1490428c85ea1b12e43033e7c33432b
Dirk Lemstra <dirk@lemstra.org>
2022-12-20 19:06:45 +0100

    Implemented day 20 part 2.

--- 2022/20/main.dart
+++ 2022/20/main.dart
@@ -6,9 +6,7 @@ class Value {
     Value(this.number);
 }
 
-void solve(List<String> lines) {
-    final original = lines.map(int.parse).map(Value.new).toList();
-    final items = List<Value>.from(original);
+void moveItems(List<Value> original, List<Value> items) {
     final length = items.length - 1;
 
     for (var value in original) {
@@ -16,16 +14,38 @@ void solve(List<String> lines) {
         items.removeAt(index);
         items.insert((index + value.number) % length, value);
     }
+}
+
+void printCoordinateSum(List<Value> items) {
     final zeroIndex = items.indexWhere((item) => item.number == 0);
-    var part1 = items[(zeroIndex + 1000) % items.length].number;
-    part1 += items[(zeroIndex + 2000) % items.length].number;
-    part1 += items[(zeroIndex + 3000) % items.length].number;
-    print(part1);
+    var sum = items[(zeroIndex + 1000) % items.length].number;
+    sum += items[(zeroIndex + 2000) % items.length].number;
+    sum += items[(zeroIndex + 3000) % items.length].number;
+    print(sum);
+}
+
+void part1(List<String> lines) {
+    final original = lines.map(int.parse).map(Value.new).toList();
+    final items = List<Value>.from(original);
+
+    moveItems(original, items);
+    printCoordinateSum(items);
+}
+
+void part2(List<String> lines) {
+    final original = lines.map(int.parse).map((value) => value * 811589153).map(Value.new).toList();
+    final items = List<Value>.from(original);
+
+    for (var i = 0; i < 10; i++) {
+        moveItems(original, items);
+    }
+    printCoordinateSum(items);
 }
 
 void main() {
     final input = new File("input");
     final lines = input.readAsLinesSync();
 
-    solve(lines);
+    part1(lines);
+    part2(lines);
 }
