dab051072b85aaca8409a54b0294925b832e37c9
Dirk Lemstra <dirk@lemstra.org>
2022-12-21 14:21:38 +0100

    Implemented day 21 part 2.

--- 2022/21/main.dart
+++ 2022/21/main.dart
@@ -4,51 +4,49 @@ enum Operation {
     plus,
     minus,
     divide,
-    multiply
-}
-
-String? operationValue(Operation? operation) {
-    switch(operation) {
-        case null: return null;
-        case Operation.plus: return "+";
-        case Operation.minus: return "-";
-        case Operation.divide: return "/";
-        case Operation.multiply: return "*";
-    }
+    multiply,
+    equal
 }
 
 class Monkey {
     final String name;
-    int value = 0;
+    double value = 0;
     bool done = false;
-    String? left;
+    String? leftName;
+    String? rightName;
     Operation? operation;
-    String? right;
+    Monkey? left;
+    Monkey? right;
 
     Monkey(this.name);
 
+    void init(List<Monkey> monkeys) {
+        if (operation != null) {
+            left = monkeys.firstWhere((monkey) => monkey.name == leftName);
+            right = monkeys.firstWhere((monkey) => monkey.name == rightName);
+        }
+        reset();
+    }
+
+    void reset() {
+        done = operation == null;
+        if (!done) value = 0;
+    }
+
     void yell(List<Monkey> monkeys) {
         if (done) return;
-
-        final a = monkeys.firstWhere((monkey) => monkey.name == left);
-        if (!a.done) return;
-
-        final b = monkeys.firstWhere((monkey) => monkey.name == right);
-        if (!b.done) return;
+        if (!left!.done) return;
+        if (!right!.done) return;
 
         switch(operation) {
-            case Operation.plus: value = a.value + b.value; break;
-            case Operation.minus: value = a.value - b.value; break;
-            case Operation.divide: value = (a.value / b.value).floor(); break;
-            case Operation.multiply: value = a.value * b.value; break;
+            case Operation.plus: value = left!.value + right!.value; break;
+            case Operation.minus: value = left!.value - right!.value; break;
+            case Operation.divide: value = left!.value / right!.value; break;
+            case Operation.multiply: value = left!.value * right!.value; break;
         }
 
         done = true;
     }
-
-    @override
-    String toString()
-        => "${name} = ${value} ${left ?? ""} ${operationValue(operation) ?? ""} ${right ?? ""}".trim();
 }
 
 List<Monkey> loadMonkeys(List<String> lines) {
@@ -56,14 +54,13 @@ List<Monkey> loadMonkeys(List<String> lines) {
     for (var line in lines) {
         var info = line.split(": ");
         final monkey = new Monkey(info[0]);
-        final value = int.tryParse(info[1]);
+        final value = double.tryParse(info[1]);
         if (value != null) {
             monkey.value = value;
-            monkey.done = true;
         } else {
             info = info[1].split(" ");
-            monkey.left = info[0];
-            monkey.right = info[2];
+            monkey.leftName = info[0];
+            monkey.rightName = info[2];
             switch(info[1]) {
                 case "+": monkey.operation = Operation.plus; break;
                 case "-": monkey.operation = Operation.minus; break;
@@ -75,7 +72,66 @@ List<Monkey> loadMonkeys(List<String> lines) {
         result.add(monkey);
     }
 
+    for (var monkey in result) {
+        monkey.init(result);
+    }
+
     return result;
+}
+
+List<Monkey> getMonkeysWithOperation(List<Monkey> monkeys) {
+    final result = List<Monkey>.from(monkeys);
+    result.retainWhere((monkey) => monkey.operation != null);
+    result.sort((a, b) => a.left == b || a.right == b ? -1 : 1);
+    return result;
+}
+
+void makeMonkeysYell(List<Monkey> monkeys, Monkey root, List<Monkey> monkeysWithOperation) {
+    for (final monkey in monkeysWithOperation) {
+        monkey.reset();
+    }
+
+    while (!root.done) {
+        for (final monkey in monkeysWithOperation) {
+            monkey.yell(monkeys);
+        }
+    }
+}
+
+void part1(Monkey root, List<Monkey> monkeys, List<Monkey> monkeysWithOperation) {
+    makeMonkeysYell(monkeys, root, monkeysWithOperation);
+    print(root.value.floor());
+}
+
+void part2(Monkey root, List<Monkey> monkeys, List<Monkey> monkeysWithOperation) {
+    makeMonkeysYell(monkeys, root, monkeysWithOperation);
+
+    root.operation = Operation.equal;
+
+    final me = monkeys.firstWhere((monkey) => monkey.name == "humn");
+
+    while (root.left!.value > root.right!.value) {
+        me.value = me.value * 2;
+
+        makeMonkeysYell(monkeys, root, monkeysWithOperation);
+    }
+
+    var min = me.value / 2;
+    var max = me.value;
+
+    while (root.left!.value != root.right!.value) {
+        me.value = min + ((max - min) / 2).floor();
+
+        makeMonkeysYell(monkeys, root, monkeysWithOperation);
+
+        if (root.left!.value > root.right!.value) {
+            min = me.value;
+        } else {
+            max = me.value;
+        }
+    }
+
+    print(me.value.floor());
 }
 
 void main() {
@@ -83,17 +139,9 @@ void main() {
     final lines = input.readAsLinesSync();
 
     final monkeys = loadMonkeys(lines);
+    final monkeysWithOperation = getMonkeysWithOperation(monkeys);
+    final root = monkeys.firstWhere((monkey) => monkey.name == "root");
 
-    final monkeysWithOperation = List<Monkey>.from(monkeys);
-    monkeysWithOperation.retainWhere((monkey) => monkey.operation != null);
-    monkeysWithOperation.sort((a, b) => a.left == b || a.right == b ? -1 : 1);
-
-    final root = monkeysWithOperation.firstWhere((monkey) => monkey.name == "root");
-    while (!root.done) {
-        for (final monkey in monkeysWithOperation) {
-            monkey.yell(monkeys);
-        }
-    }
-
-    print(root.value);
+    part1(root, monkeys, monkeysWithOperation);
+    part2(root, monkeys, monkeysWithOperation);
 }
