498d7a01e4256ee0b11187754cf49858bcd0e1ad
Dirk Lemstra <dirk@lemstra.org>
2022-12-22 20:37:01 +0100

    Implemented day 22 part 2.

--- 2022/22/main.dart
+++ 2022/22/main.dart
@@ -7,20 +7,63 @@ enum Direction {
     up
 }
 
-int getNextY(int y, Direction direction) {
+class Position {
+    int x = 0;
+    int y = 0;
+
+    Position(this.x, this.y);
+
+    Position.from(Position other) {
+        this.x = other.x;
+        this.y = other.y;
+    }
+
+    @override
+    String toString()
+        => "${x}x${y}";
+}
+
+Position nextPosition(Position position, Direction direction) {
     switch (direction) {
-        case Direction.up: return y - 1;
-        case Direction.down: return y + 1;
-        default: return y;
+        case Direction.up: return new Position(position.x, position.y - 1);
+        case Direction.down: return new Position(position.x, position.y + 1);
+        case Direction.left: return new Position(position.x - 1, position.y);
+        case Direction.right: return new Position(position.x + 1, position.y);
     }
 }
 
-int getNextX(int x, Direction direction) {
-    switch (direction) {
-        case Direction.left: return x - 1;
-        case Direction.right: return x + 1;
-        default: return x;
+List nextCubePosition(Position position, Direction direction, int size) {
+     //  12
+     //  3
+     // 45
+     // 6
+
+    switch(direction) {
+        case Direction.up:
+            if (position.x < size) return [new Position(size, size + position.x), Direction.right]; // 4U => 3R
+            if (position.x < size * 2) return [new Position(0, (size * 2) + position.x), Direction.right]; // 1U => 6L
+            if (position.x < size * 3) return [new Position(position.x - (size * 2), (size * 4) - 1), Direction.up]; // 2U => 6D
+            break;
+        case Direction.down:
+            if (position.x < size) return [new Position((size * 2) + position.x, 0), Direction.down]; // 6D => 2U
+            if (position.x < size * 2) return [new Position(size - 1, (size * 2) + position.x), Direction.left]; // 5D => 6L
+            if (position.x < size * 3) return [new Position((size * 2) - 1, position.x - size), Direction.left];  // 2D => 3R
+            break;
+        case Direction.left:
+            if (position.y < size) return [new Position(0, (size * 3) - 1 - position.y), Direction.right]; // 1L => 4L
+            if (position.y < size * 2) return [new Position(position.y - size, size * 2), Direction.down]; // 3L => 4U
+            if (position.y < size * 3) return [new Position(size, (size * 3) - 1 - position.y), Direction.right]; // 4L => 1L
+            if (position.y < size * 4) return [new Position(position.y - (size * 2), 0), Direction.down]; // 6L => 1U
+            break;
+        case Direction.right:
+            if (position.y < size) return [new Position((size * 2) - 1, (size * 3) - 1 - position.y), Direction.left]; // 2R => 5R
+            if (position.y < size * 2) return [new Position(size + position.y, size - 1), Direction.up]; // 3R => 2D
+            if (position.y < size * 3) return [new Position((size * 3) - 1, (size * 3) - 1 - position.y), Direction.left]; // 5R => 2R
+            if (position.y < size * 4) return [new Position(position.y - (size * 2), (size * 3) - 1), Direction.up]; // 6R => 5D
+            break;
     }
+
+    throw new Exception();
 }
 
 Direction turnAround(Direction direction) {
@@ -32,30 +75,18 @@ Direction turnAround(Direction direction) {
     }
 }
 
-solve(List<String> lines) {
-    var walls = new Set<String>();
-    var tiles = new Set<String>();
-
-    var y = 0;
-    var xPos = 0;
-    var yPos = lines.length * 100;
-    for (final line in lines) {
-        for (var x = 0; x < line.length; x++) {
-            if (line[x] == '#') walls.add("${x}x${y}");
-            else if (line[x] == '.') {
-                tiles.add("${x}x${y}");
-                if (y < yPos) {
-                    xPos = x;
-                    yPos = y;
-                }
-            }
-        }
-        y++;
-        if (line == "") break;
+Position wrapAround(Set<String> tiles, Set<String> walls, Position position, Direction direction) {
+    direction = turnAround(direction);
+    var pos = Position.from(position);
+    while (walls.contains(pos.toString()) || tiles.contains(pos.toString())) {
+        pos = nextPosition(pos, direction);
     }
+    direction = turnAround(direction);
+    pos = nextPosition(pos, direction);
+    return pos;
+}
 
-    final route = lines[y].split("R").join(" R ").split("L").join(" L ").split(" ");
-
+void move(Set<String> tiles, Set<String> walls, List<String> route, Position position, int cubeSize) {
     var direction = Direction.right;
     for (var i = 0; i < route.length; i ++) {
         switch (route[i]) {
@@ -68,39 +99,61 @@ solve(List<String> lines) {
             default:
                 var count = int.parse(route[i]);
                 while (count-- != 0) {
-                    var x = getNextX(xPos, direction);
-                    var y = getNextY(yPos, direction);
-                    var pos = "${x}x${y}";
-                    if (tiles.contains(pos)) {
-                        xPos = x;
-                        yPos = y;
-                    } else if (walls.contains(pos)) {
+                    var pos = nextPosition(position, direction);
+                    if (tiles.contains(pos.toString())) {
+                        position = pos;
+                    } else if (walls.contains(pos.toString())) {
                         break;
                     } else {
-                        direction = turnAround(direction);
-                        x = getNextX(xPos, direction);
-                        y = getNextY(yPos, direction);
-                        pos = "${x}x${y}";
-                        while ((walls.contains(pos) || tiles.contains(pos)) && x >= 0 && y >= 0) {
-                            x = getNextX(x, direction);
-                            y = getNextY(y, direction);
-                            pos = "${x}x${y}";
+                        var newDirection = direction;
+                        if (cubeSize > 0) {
+                            final info = nextCubePosition(position, direction, cubeSize);
+                            pos = info[0];
+                            newDirection = info[1];
+                        } else {
+                            pos = wrapAround(tiles, walls, position, direction);
                         }
-                        direction = turnAround(direction);
-                        x = getNextX(x, direction);
-                        y = getNextY(y, direction);
-                        if (walls.contains("${x}x${y}")) {
+                        if (walls.contains(pos.toString())) {
                             break;
                         }
-                        xPos = x;
-                        yPos = y;
+
+                        position = pos;
+                        direction = newDirection;
                     }
                 }
                 break;
         }
     }
 
-    print(1000 * (yPos + 1) + 4 * (xPos + 1) + direction.index);
+    print(1000 * (position.y + 1) + 4 * (position.x + 1) + direction.index);
+}
+
+solve(List<String> lines) {
+    final walls = new Set<String>();
+    final tiles = new Set<String>();
+
+    final pos = new Position(0, 100);
+    var y = 0;
+    for (final line in lines) {
+        for (var x = 0; x < line.length; x++) {
+            if (line[x] == '#') walls.add("${x}x${y}");
+            else if (line[x] == '.') {
+                tiles.add("${x}x${y}");
+                if (y < pos.y) {
+                    pos.x = x;
+                    pos.y = y;
+                }
+            }
+        }
+        y++;
+        if (line == "") break;
+    }
+
+    final route = lines[y].split("R").join(" R ").split("L").join(" L ").split(" ");
+    final cubeSize = ((lines.length -2 ) / 4).floor();
+
+    move(tiles, walls, route, pos, 0);
+    move(tiles, walls, route, pos, cubeSize);
 }
 
 void main() {
