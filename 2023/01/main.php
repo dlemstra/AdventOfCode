ed660094e112e6ef9149326dd384d731c37f0d13
Dirk Lemstra <dirk@lemstra.org>
2023-12-01 11:37:47 +0100

    Implemented day 1 part 2.

--- 2023/01/main.php
+++ 2023/01/main.php
@@ -26,3 +26,40 @@ foreach ($input as $line) {
 }
 
 echo $total . "\n";
+
+function getAllIndexes($haystack, $needle) {
+    $pos = 0;
+    $allpos = [];
+
+    while (($pos = strpos($haystack, $needle, $pos)) !== false) {
+        $allpos[] = $pos++;
+    }
+
+    return $allpos;
+}
+
+$total = 0;
+$numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+foreach ($input as $line) {
+    $indexes = [];
+
+    for ($i = 0; $i < strlen($line); $i++) {
+        if (is_numeric($line[$i])) {
+            $indexes[$i] = $line[$i];
+        }
+    }
+
+    for ($i = 0; $i < count($numbers); ++$i) {
+       foreach (getAllIndexes($line, $numbers[$i]) as $index) {
+           $indexes[$index] = $i + 1;
+       }
+    }
+
+    ksort($indexes);
+
+    $keys = array_keys($indexes);
+    $total += $indexes[$keys[0]] * 10;
+    $total += $indexes[$keys[count($keys) - 1]];
+}
+
+echo $total . "\n";
