cebc152538f837ede92870deb139cd538a054f60
Dirk Lemstra <dirk@lemstra.org>
2023-12-06 06:59:33 +0100

    Implemented day 6 part 2.

--- 2023/06/main.php
+++ 2023/06/main.php
@@ -2,6 +2,31 @@
 
 $lines = file('input', FILE_IGNORE_NEW_LINES);
 
+function countWins($time, $record) {
+    $speed = 2;
+    $start = 0;
+    for( $i = $speed; $i < $time; $i++, $speed++) {
+        $remaining = $time - $i;
+        if ($remaining * $speed > $record) {
+            $start = $i;
+            break;
+        }
+    }
+
+    $speed = $time - 1;
+    $end = 0;
+    for( $i = $speed; ; $i--, $speed--) {
+        $remaining = $time - $i;
+        if ($remaining * $speed > $record) {
+            $end = $i;
+            break;
+        }
+    }
+
+    return $end - $start + 1;
+}
+
+
 $times = preg_split('/\s+/', trim(explode('Time:', $lines[0])[1]));
 $distances = preg_split('/\s+/', trim(explode('Distance:', $lines[1])[1]));
 
@@ -10,16 +35,13 @@ for ($i = 0; $i < count($distances); $i++) {
     $time = $times[$i];
     $record = $distances[$i];
 
-    $count = 0;
-    $speed = 2;
-    for( $j = 2; $j < $time; $j++) {
-        $remaining = $time - $j;
-        if ($remaining * $speed > $record) {
-            $count++;
-        }
-        $speed++;
-    }
-    $part1 *= $count;
+    $part1 *= countWins($time, $record);
 }
 
 echo $part1 . "\n";
+
+$time = join("", $times);
+$record = join("", $distances);
+$part2 = countWins($time, $record);
+
+echo $part2 . "\n";
