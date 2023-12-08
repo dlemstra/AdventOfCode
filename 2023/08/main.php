c06b87a6d4ab093130adcbd84a0c5689b96b436f
Dirk Lemstra <dirk@lemstra.org>
2023-12-08 11:04:17 +0100

    Implemented day 8 part 2.

--- 2023/08/main.php
+++ 2023/08/main.php
@@ -11,11 +11,35 @@ class Node
     }
 }
 
+function countMovesToExit(array $nodes, string $id, string $moves, callable $callback)
+{
+    $i = 0;
+    $node = $nodes[$id];
+    while (true) {
+        $direction = $moves[$i % strlen($moves)];
+        $node = $nodes[$node->connections[$direction]];
+        $i++;
+        if ($callback($node->id) === true) {
+            return $i;
+        }
+    }
+}
+
+function lcm(int $a, int $b)
+{
+    $gcd = function ($a, $b) use (&$gcd) {
+        return ($a % $b) ? $gcd($b, $a % $b) : $b;
+    };
+
+    return abs($a * $b) / $gcd($a, $b);
+}
+
 $lines = file('input', FILE_IGNORE_NEW_LINES);
 
 $moves = $lines[0];
 
 $nodes = [];
+$starts = [];
 for ($i = 2; $i < count($lines); $i++) {
     $line = $lines[$i];
     $info = explode(' = ', $line);
@@ -25,14 +49,25 @@ for ($i = 2; $i < count($lines); $i++) {
     $node->connections['L'] = $connections[0];
     $node->connections['R'] = $connections[1];
     $nodes[$id] = $node;
+    if ($id[2] == 'A') {
+        $starts[] = $id;
+    }
 }
 
-$node = $nodes['AAA'];
-$i = 0;
-while ($node->id != 'ZZZ') {
-    $direction = $moves[$i % strlen($moves)];
-    $node = $nodes[$node->connections[$direction]];
-    $i++;
+$part1 = countMovesToExit($nodes, 'AAA', $moves, function ($id) {
+    return $id == 'ZZZ';
+});
+
+echo $part1 . "\n";
+
+$part2 = countMovesToExit($nodes, $starts[0], $moves, function ($id) {
+    return $id[2] == 'Z';
+});
+
+for ($i = 1; $i < count($starts); $i++) {
+    $part2 = lcm($part2, countMovesToExit($nodes, $starts[$i], $moves, function ($id) {
+        return $id[2] == 'Z';
+    }));
 }
 
-echo $i . "\n";
+echo $part2 . "\n";
