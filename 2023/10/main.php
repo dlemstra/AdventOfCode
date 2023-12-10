b58340260223c57bffcac766ef9e8c56257c7582
Dirk Lemstra <dirk@lemstra.org>
2023-12-10 14:32:45 +0100

    Implemented day 10 part 2.

--- 2023/10/main.php
+++ 2023/10/main.php
@@ -1,6 +1,6 @@
 <?php
 
-function getStartNext(array $grid, int $x, int $y): array
+function getConnected(array $grid, int $x, int $y): array
 {
     if ($grid[$y][$x + 1] === "J" || $grid[$y][$x + 1] === "-" || $grid[$y][$x + 1] === "7") {
         return [$x + 1, $y];
@@ -14,6 +14,8 @@ function getStartNext(array $grid, int $x, int $y): array
     if ($grid[$y - 1][$x] === "7" || $grid[$y - 1][$x] === "|" || $grid[$y - 1][$x] === "F") {
         return [$x, $y - 1];
     }
+
+    throw new Exception("Invalid position");
 }
 
 function getNextPosition(array $grid, int $prevX, int $prevY, int $x, int $y): array
@@ -33,7 +35,83 @@ function getNextPosition(array $grid, int $prevX, int $prevY, int $x, int $y): a
             return $x + 1 == $prevX ? [$x, $y + 1] : [$x + 1, $y];
     }
 
-    return [];
+    throw new Exception("Invalid position");
+}
+
+function expandGrid(array $grid): array
+{
+    $newGrid = [array_fill(0, (count($grid[0]) * 2) - 3, ".")];
+    for ($y = 1; $y < count($grid) - 1; $y++) {
+        $newRow = ["."];
+        for ($x = 1; $x < count($grid[$y]) - 1; $x++) {
+            $newRow[] = $grid[$y][$x];
+            $nextChar = ".";
+            if ($grid[$y][$x] != ".") {
+                if (($grid[$y][$x] === "L" || $grid[$y][$x] === "-" || $grid[$y][$x] === "F" || $grid[$y][$x] === "S") &&
+                    ($grid[$y][$x + 1] === "J" || $grid[$y][$x + 1] === "-" || $grid[$y][$x + 1] === "7" || $grid[$y][$x + 1] === "S")
+                ) {
+                    $nextChar = "-";
+                }
+            }
+            $newRow[] = $nextChar;
+        }
+        $newGrid[] = $newRow;
+
+        $newRow = ["."];
+        for ($x = 1; $x < count($grid[$y]) - 1; $x++) {
+            $nextChar = ".";
+            if (
+                ($grid[$y][$x] === "7" || $grid[$y][$x] === "|" || $grid[$y][$x] === "F" || $grid[$y][$x] === "S") &&
+                ($grid[$y + 1][$x] === "L" || $grid[$y + 1][$x] === "|" || $grid[$y + 1][$x] === "J" || $grid[$y + 1][$x] === "S")
+            ) {
+                $nextChar = "|";
+            }
+            $newRow[] = $nextChar;
+            $newRow[] = ".";
+        }
+        $newGrid[] = $newRow;
+    }
+
+    return $newGrid;
+}
+
+function findPath(array &$grid, int $startX, int $startY, string $replacement = ""): int
+{
+    $length = 0;
+    $prevX = $startX;
+    $prevY = $startY;
+    $next = getConnected($grid, $startX, $startY);
+    while ($grid[$next[1]][$next[0]] !== "S") {
+        $prev = $next;
+        $next = getNextPosition($grid, $prevX, $prevY, $next[0], $next[1]);
+        $prevX = $prev[0];
+        $prevY = $prev[1];
+        if ($replacement !== "") {
+            $grid[$prevY][$prevX] = $replacement;
+        }
+        $length++;
+    }
+
+    return $length;
+}
+
+function floodFill(array &$grid)
+{
+    $maxX = count($grid[0]) - 1;
+    $maxY = count($grid) - 1;
+    $positions = [[0, 0]];
+    while (count($positions) > 0) {
+        $position = array_pop($positions);
+        $x = $position[0];
+        $y = $position[1];
+        if ($grid[$y][$x] !== "S" && $grid[$y][$x] !== "O") {
+            $grid[$y][$x] = "O";
+            if ($x < $maxX) $positions[] = [$x + 1, $y];
+            if ($x > 0) $positions[] = [$x - 1, $y];
+            if ($y < $maxY) $positions[] = [$x, $y + 1];
+            if ($y > 0) $positions[] = [$x, $y - 1];
+        }
+    }
 }
 
 $lines = file('input', FILE_IGNORE_NEW_LINES);
@@ -54,19 +132,21 @@ foreach ($lines as $line) {
 }
 $grid[] = array_fill(0, strlen($lines[0]) + 2, ".");
 
-$count = 0;
-$prevX = $startX;
-$prevY = $startY;
-$next = getStartNext($grid, $startX, $startY);
-while ($grid[$next[1]][$next[0]] !== "S") {
-    $prev = $next;
-    $next = getNextPosition($grid, $prevX, $prevY, $next[0], $next[1]);
-    if (count($next) === 0) {
-        break;
+$part1 = findPath($grid, $startX, $startY);
+echo ($part1 + 1) / 2 . "\n";
+
+$grid = expandGrid($grid);
+findPath($grid, ($startX * 2) - 1, ($startY * 2) - 1, "S");
+
+floodFill($grid);
+
+$part2 = 0;
+for ($y = 1; $y < count($grid); $y += 2) {
+    for ($x = 1; $x < count($grid[$y]); $x += 2) {
+        if ($grid[$y][$x] !== "S" && $grid[$y][$x] !== "O") {
+            $part2++;
+        }
     }
-    $prevX = $prev[0];
-    $prevY = $prev[1];
-    $count++;
 }
 
-echo ($count + 1) / 2 . "\n";
+echo $part2 . "\n";
