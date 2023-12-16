a61198288400916c14b6e877ba44e5a3a7e848d4
Dirk Lemstra <dirk@lemstra.org>
2023-12-16 10:06:23 +0100

    Implemented day 16 part 2.

--- 2023/16/main.php
+++ 2023/16/main.php
@@ -21,14 +21,9 @@ class Move
         $this->direction = $direction;
     }
 
-    function position(): string
-    {
-        return $this->x . 'x' . $this->y;
-    }
-
     function addToVisited(array &$visited): bool
     {
-        $key = $this->position();
+        $key = $this->x . 'x' . $this->y;
         if (!isset($visited[$key])) {
             $visited[$key] = [$this->direction];
             return true;
@@ -50,8 +45,8 @@ class Move
         $newMoves = [];
         switch ($grid[$y][$x]) {
             case '|':
-                if ($this->direction == Direction::North || $this->direction == Direction::South) {
-                    $y = $this->direction == Direction::North ? $y - 1 : $y + 1;
+                if ($this->direction === Direction::North || $this->direction === Direction::South) {
+                    $y = $this->direction === Direction::North ? $y - 1 : $y + 1;
                     $newMoves[] = new Move($x, $y, $this->direction);
                 } else {
                     $newMoves[] = new Move($x, $y - 1, Direction::North);
@@ -59,8 +54,8 @@ class Move
                 }
                 break;
             case '-':
-                if ($this->direction == Direction::East || $this->direction == Direction::West) {
-                    $x = $this->direction == Direction::East ? $x + 1 : $x - 1;
+                if ($this->direction === Direction::East || $this->direction === Direction::West) {
+                    $x = $this->direction === Direction::East ? $x + 1 : $x - 1;
                     $newMoves[] = new Move($x, $y, $this->direction);
                 } else {
                     $newMoves[] = new Move($x - 1, $y, Direction::West);
@@ -68,46 +63,40 @@ class Move
                 }
                 break;
             case '/':
-                if ($this->direction == Direction::North) {
+                if ($this->direction === Direction::North)
                     $newMoves[] = new Move($x + 1, $y, Direction::East);
-                } elseif ($this->direction == Direction::East) {
+                elseif ($this->direction === Direction::East)
                     $newMoves[] = new Move($x, $y - 1, Direction::North);
-                } elseif ($this->direction == Direction::South) {
+                elseif ($this->direction === Direction::South)
                     $newMoves[] = new Move($x - 1, $y, Direction::West);
-                } elseif ($this->direction == Direction::West) {
+                elseif ($this->direction === Direction::West)
                     $newMoves[] = new Move($x, $y + 1, Direction::South);
-                }
                 break;
             case '\\':
-                if ($this->direction == Direction::North) {
+                if ($this->direction === Direction::North)
                     $newMoves[] = new Move($x - 1, $y, Direction::West);
-                } elseif ($this->direction == Direction::East) {
+                elseif ($this->direction === Direction::East)
                     $newMoves[] = new Move($x, $y + 1, Direction::South);
-                } elseif ($this->direction == Direction::South) {
+                elseif ($this->direction === Direction::South)
                     $newMoves[] = new Move($x + 1, $y, Direction::East);
-                } elseif ($this->direction == Direction::West) {
+                elseif ($this->direction === Direction::West)
                     $newMoves[] = new Move($x, $y - 1, Direction::North);
-                }
                 break;
             default:
-                if ($this->direction == Direction::North) {
+                if ($this->direction === Direction::North)
                     $newMoves[] = new Move($x, $y - 1, Direction::North);
-                } elseif ($this->direction == Direction::East) {
+                elseif ($this->direction === Direction::East)
                     $newMoves[] = new Move($x + 1, $y, Direction::East);
-                } elseif ($this->direction == Direction::South) {
+                elseif ($this->direction === Direction::South)
                     $newMoves[] = new Move($x, $y + 1, Direction::South);
-                } elseif ($this->direction == Direction::West) {
+                elseif ($this->direction === Direction::West)
                     $newMoves[] = new Move($x - 1, $y, Direction::West);
-                }
                 break;
         }
 
         foreach ($newMoves as $newMove) {
-            if (!$newMove->isValid($maxY, $maxX)) continue;
-
-            if ($newMove->addToVisited($visited)) {
+            if ($newMove->isValid($maxY, $maxX) && $newMove->addToVisited($visited))
                 yield $newMove;
-            }
         }
     }
 
@@ -117,20 +106,39 @@ class Move
     }
 }
 
+function energizeTiles(array $grid, int $maxX, int $maxY, Move $move): int
+{
+    $visited = [];
+
+    $moves[] = $move;
+    $moves[0]->addToVisited($visited);
+
+    while (count($moves) > 0) {
+        $move = array_shift($moves);
+        foreach ($move->nextMoves($visited, $grid, $maxX, $maxY) as $nextMove) {
+            $moves[] = $nextMove;
+        }
+    }
+
+    return count($visited);
+}
+
 $grid = file('input', FILE_IGNORE_NEW_LINES);
 
 $maxX = strlen($grid[0]);
 $maxY = count($grid);
 
-$visited = [];
-$moves[] = new Move(0, 0, Direction::East);
-$moves[0]->addToVisited($visited);
+$part1 = energizeTiles($grid, $maxX, $maxY, new Move(0, 0, Direction::East));
+echo $part1 . "\n";
 
-while (count($moves) > 0) {
-    $move = array_shift($moves);
-    foreach ($move->nextMoves($visited, $grid, $maxX, $maxY) as $nextMove) {
-        $moves[] = $nextMove;
-    }
+$part2 = 0;
+for ($y = 0; $y < $maxY; $y++) {
+    $part2 = max($part2, energizeTiles($grid, $maxX, $maxY, new Move(0, $y, Direction::East)));
+    $part2 = max($part2, energizeTiles($grid, $maxX, $maxY, new Move($maxX - 1, $y, Direction::West)));
+}
+for ($x = 0; $x < $maxX; $x++) {
+    $part2 = max($part2, energizeTiles($grid, $maxX, $maxY, new Move($x, 0, Direction::South)));
+    $part2 = max($part2, energizeTiles($grid, $maxX, $maxY, new Move($x, $maxY - 1, Direction::North)));
 }
 
-echo count($visited) . "\n";
+echo $part2 . "\n";
