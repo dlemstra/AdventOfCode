81e8da5643161404dd3d8cd093bb5b7f9c682f60
Dirk Lemstra <dirk@lemstra.org>
2023-12-18 19:02:56 +0100

    Implemented day 18 part 2.

--- 2023/18/main.php
+++ 2023/18/main.php
@@ -1,80 +1,62 @@
 <?php
 
-function floodFill(array &$grid, $minX, $minY, $maxX, $maxY)
+function polygonArea($vertices)
 {
-    $positions = [[$minX, $minY]];
-    while (count($positions) > 0) {
-        $position = array_pop($positions);
-        $x = $position[0];
-        $y = $position[1];
-        if (!isset($grid[$y]))
-            $grid[$y] = [];
-
-        if (isset($grid[$y][$x]))
-            continue;
-
-        $grid[$y][$x] = 0;
-        if ($x < $maxX) $positions[] = [$x + 1, $y];
-        if ($x > $minX) $positions[] = [$x - 1, $y];
-        if ($y < $maxY) $positions[] = [$x, $y + 1];
-        if ($y > $minY) $positions[] = [$x, $y - 1];
+    $area = 0;
+    for ($i = 0; $i < count($vertices) - 1; $i++) {
+        $area = $area + ($vertices[$i + 1][0] + $vertices[$i][0]) * ($vertices[$i + 1][1] - $vertices[$i][1]);
     }
+
+    return abs($area / 2);
+}
+
+function getAreaSize(array $lines, bool $part1)
+{
+    $points = [];
+
+    $x = 0;
+    $y = 0;
+    $border = 0;
+    foreach ($lines as $line) {
+        $parts = explode(' ', $line);
+        if ($part1) {
+            $direction = $parts[0];
+            $length = (int)$parts[1];
+        } else {
+            $direction = $parts[2][7];
+            $length = hexdec(substr($parts[2], 2, 5));
+        }
+        $border += $length;
+        switch ($direction) {
+            case 'R':
+            case '0':
+                $points[] = [$x, $y];
+                $x += $length;
+                break;
+            case 'D':
+            case '1':
+                $points[] = [$x, $y];
+                $y += $length;
+                break;
+            case 'L':
+            case '2':
+                $points[] = [$x, $y];
+                $x -= $length;
+                break;
+            case 'U':
+            case '3':
+                $points[] = [$x, $y];
+                $y -= $length;
+                break;
+        }
+    }
+
+    $area = polygonArea($points);
+    $area += ($border / 2) + 1;
+    return $area;
 }
 
 $lines = file('input', FILE_IGNORE_NEW_LINES);
 
-$index = 0;
-
-$grid = [];
-$x = 0;
-$y = 0;
-$minX = $maxX = $minY = $maxY = 0;
-foreach ($lines as $line) {
-    $parts = explode(' ', $line);
-    switch ($parts[0]) {
-        case 'R':
-            for ($i = 0; $i < $parts[1]; $i++) {
-                $grid[$y][$x] = "#";
-                $x++;
-                $maxX = max($maxX, $x);
-            }
-            break;
-        case 'L':
-            for ($i = 0; $i < $parts[1]; $i++) {
-                $grid[$y][$x] = "#";
-                $x--;
-                $minX = min($minX, $x);
-            }
-            break;
-        case 'U':
-            for ($i = 0; $i < $parts[1]; $i++) {
-                $grid[$y][$x] = "#";
-                $y--;
-                $minY = min($minY, $y);
-            }
-            break;
-        case 'D':
-            for ($i = 0; $i < $parts[1]; $i++) {
-                $grid[$y][$x] = "#";
-                $y++;
-                $maxY = max($maxY, $y);
-            }
-            break;
-    }
-}
-
-$maxX++;
-$maxY++;
-
-floodFill($grid, $minX - 1, $minY - 1, $maxX + 1, $maxY + 1);
-
-$part1 = 0;
-for ($y = $minY; $y < $maxY; $y++) {
-    for ($x = $minX; $x < $maxX; $x++) {
-        if (!isset($grid[$y][$x]) || $grid[$y][$x] !== 0) {
-            $part1++;
-        }
-    }
-}
-
-echo $part1 . "\n";
+echo getAreaSize($lines, true) . "\n";
+echo getAreaSize($lines, false) . "\n";
