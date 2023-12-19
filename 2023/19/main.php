f331eb71ed212fef23ee81964b90ddae1c00f5c0
Dirk Lemstra <dirk@lemstra.org>
2023-12-19 08:09:18 +0100

    Implemented day 19 part 1.

---
+++ 2023/19/main.php
@@ -0,0 +1,98 @@
+<?php
+
+class Rule
+{
+    public readonly string $target;
+    private readonly string $part;
+    private readonly int $value;
+    private readonly bool $checkLess;
+
+    public function __construct(string $target, string $part = "", bool $checkLess = false, int $value = 0)
+    {
+        $this->target = $target;
+        $this->part = $part;
+        $this->value = $value;
+        $this->checkLess = $checkLess;
+    }
+
+    public static function parse(string $rule): Rule
+    {
+        $parts = explode(':', $rule);
+        if (count($parts) == 1)
+            return new Rule($parts[0]);
+
+        $target = $parts[1];
+        $part = $parts[0][0];
+        $checkLess = $parts[0][1] === '<';
+        $value = (int)substr($parts[0], 2);
+
+        return new Rule($target, $part, $checkLess, $value);
+    }
+
+    public function matches(array $values): bool
+    {
+        return $this->part === "" ||
+            ($this->checkLess && $values[$this->part] < $this->value) ||
+            (!$this->checkLess && $values[$this->part] > $this->value);
+    }
+}
+
+class WorkFlow
+{
+    public readonly string $name;
+    private array $rules;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function addRule(Rule $rule): void
+    {
+        $this->rules[] = $rule;
+    }
+
+    public function getNextTarget(array $values): string
+    {
+        foreach ($this->rules as $rule) {
+            if ($rule->matches($values))
+                return $rule->target;
+        }
+        throw new Exception("No matching rule found");
+    }
+}
+
+$lines = file('input', FILE_IGNORE_NEW_LINES);
+
+$workflows = [];
+foreach ($lines as $index => $line) {
+    if ($line === '')
+        break;
+
+    $parts = explode('{', substr($line, 0, -1));
+    $workflow = new WorkFlow($parts[0]);
+    foreach (explode(',', $parts[1]) as $rule) {
+        $workflow->addRule(Rule::parse($rule));
+    }
+    $workflows[$workflow->name] = $workflow;
+}
+
+$part1 = 0;
+for ($i = $index + 1; $i < count($lines); $i++) {
+    $line = substr($lines[$i], 1, -1);
+    $parts = explode(',', $line);
+    $values = [];
+    foreach ($parts as $part) {
+        $values[$part[0][0]] = (int)substr($part, 2);
+    }
+
+    $target = $workflows["in"]->getNextTarget($values);
+    while ($target !== "A" && $target !== "R") {
+        $target = $workflows[$target]->getNextTarget($values);
+    }
+
+    if ($target === "A")
+        $part1 += array_sum($values);
+}
+
+echo $part1 . "\n";
