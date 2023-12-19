fff7fd7310438dae70848b33c00140cf523d4090
Dirk Lemstra <dirk@lemstra.org>
2023-12-19 21:11:11 +0100

    Implemented day 19 part 2.

--- 2023/19/main.php
+++ 2023/19/main.php
@@ -3,9 +3,9 @@
 class Rule
 {
     public readonly string $target;
-    private readonly string $part;
-    private readonly int $value;
-    private readonly bool $checkLess;
+    public readonly string $part;
+    public readonly int $value;
+    public readonly bool $checkLess;
 
     public function __construct(string $target, string $part = "", bool $checkLess = false, int $value = 0)
     {
@@ -37,6 +37,22 @@ class Rule
     }
 }
 
+class Range
+{
+    public readonly string $part;
+    public readonly int $min;
+    public readonly int $max;
+    public readonly string $target;
+
+    public function __construct(string $part, int $min = 1, int $max = 4000, string $target = "in")
+    {
+        $this->part = $part;
+        $this->min = $min;
+        $this->max = $max;
+        $this->target = $target;
+    }
+}
+
 class WorkFlow
 {
     public readonly string $name;
@@ -58,8 +74,69 @@ class WorkFlow
             if ($rule->matches($values))
                 return $rule->target;
         }
+
         throw new Exception("No matching rule found");
     }
+
+    public function getRanges(array $ranges): iterable
+    {
+        foreach ($this->rules as $rule) {
+            $newRanges = [];
+            $remainingRanges = [];
+            foreach ($ranges as $range) {
+                $newRange = new Range($range->part, $range->min, $range->max, $rule->target);
+                if ($range->part !== $rule->part) {
+                    $newRanges[] = $newRange;
+                    $remainingRanges[] = $newRange;
+                } else {
+                    if ($rule->checkLess && $range->min < $rule->value) {
+                        $newRanges[] = new Range($range->part, $range->min, $rule->value - 1, $rule->target);
+                        $remainingRanges[] = new Range($range->part, $rule->value, $range->max, $range->target);
+                    } else if (!$rule->checkLess && $range->max > $rule->value) {
+                        $newRanges[] = new Range($range->part, $rule->value + 1, $range->max, $rule->target);
+                        $remainingRanges[] = new Range($range->part, $range->min, $rule->value, $range->target);
+                    } else {
+                        $newRanges[] = $newRange;
+                    }
+                }
+            }
+            yield $newRanges;
+            $ranges = $remainingRanges;
+        }
+    }
+}
+
+function countAcceptedRanges($workflows, $ranges): int
+{
+    $rangesByTarget = [];
+    foreach ($ranges as $range) {
+        $target = $range->target;
+        if (!isset($rangesByTarget[$target])) {
+            $rangesByTarget[$target] = [];
+        }
+        $rangesByTarget[$target][] = $range;
+    }
+
+    $total = 0;
+    foreach ($rangesByTarget as $name => $ranges) {
+        if ($name === "R")
+            continue;
+
+        if ($name === "A") {
+            $subTotal = 1;
+            foreach ($ranges as $range)
+                $subTotal *= $range->max - $range->min + 1;
+            $total += $subTotal;
+            continue;
+        }
+
+        $workflow = $workflows[$name];
+        foreach ($workflow->getRanges($ranges) as $newRanges) {
+            $total  += countAcceptedRanges($workflows, $newRanges);
+        }
+    }
+
+    return $total;
 }
 
 $lines = file('input', FILE_IGNORE_NEW_LINES);
@@ -96,3 +173,6 @@ for ($i = $index + 1; $i < count($lines); $i++) {
 }
 
 echo $part1 . "\n";
+
+$ranges = [new Range("x"), new Range("m"), new Range("a"), new Range("s")];
+echo countAcceptedRanges($workflows, $ranges) . "\n";
