5099ff388dd579c067d60a513ad5e68ce9b2074b
Dirk Lemstra <dirk@lemstra.org>
2024-12-01 09:45:36 +0100

    Implemented day 1 part 1.

--- 2024/01/Day1.cs
+++ 2024/01/Day1.cs
@@ -2,11 +2,36 @@ internal class Day1 : IPuzzle
 {
     public string Part1(string input)
     {
-        return "part 1";
+        var (listA, listB) = ReadInput(input);
+
+        var total = 0;
+        for (var i = 0; i < listA.Count; i++)
+        {
+            total += Math.Abs(listA[i] - listB[i]);
+        }
+
+        return total.ToString();
     }
 
     public string Part2(string input)
     {
         return "part 2";
+    }
+
+    private static (List<int>, List<int>) ReadInput(string input)
+    {
+        var listA = new List<int>();
+        var listB = new List<int>();
+        foreach (var line in input.Split('\n'))
+        {
+            var numbers = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            listA.Add(numbers[0]);
+            listB.Add(numbers[1]);
+        }
+
+        listA.Sort();
+        listB.Sort();
+        return (listA, listB);
     }
 }
