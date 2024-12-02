b4c8aaafad5b72aeb85a9392b921ccb7253fb969
Dirk Lemstra <dirk@lemstra.org>
2024-12-02 22:12:17 +0100

    Implemented day 2 part 2.

--- 2024/02/Day2.cs
+++ 2024/02/Day2.cs
@@ -6,20 +6,7 @@ internal class Day2 : IPuzzle
         foreach (var line in input.Split('\n'))
         {
             var data = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-            var safeRow = true;
-            var incrementing = data[0] < data[1];
-            for (var i = 1; i < data.Length; i++)
-            {
-                var diff = incrementing
-                    ? data[i] - data[i - 1]
-                    : data[i - 1] - data[i];
-
-                if (diff < 1 || diff > 3)
-                {
-                    safeRow = false;
-                }
-            }
-            if (safeRow)
+            if (isSafeRow(data))
                 safe++;
         }
 
@@ -28,6 +15,64 @@ internal class Day2 : IPuzzle
 
     public string Part2(string input)
     {
-        return string.Empty;
+        var safe = 0;
+        foreach (var line in input.Split('\n'))
+        {
+            var data = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            if (isSafeRow(data))
+            {
+                safe++;
+            }
+            else
+            {
+                var old = safe;
+                for (var skip = 0; skip < data.Length; skip++)
+                {
+                    if (isSafeRow(data, skip))
+                    {
+                        safe++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return safe.ToString();
+    }
+
+    private bool isSafeRow(int[] data, int skip = -1)
+    {
+        var incrementing = data[0] < data[1];
+        if (skip == 0)
+            incrementing = data[1] < data[2];
+        else if (skip == 1)
+            incrementing = data[0] < data[2];
+
+        var lastIndex = data.Length - 1;
+        for (var i = 1; i < data.Length; i++)
+        {
+            var left = incrementing ? i : i - 1;
+            var right = incrementing ? i - 1 : i;
+
+            if (skip == i)
+            {
+                continue;
+            }
+            else if (skip == i - 1)
+            {
+                if (i == 1)
+                    continue;
+
+                left = incrementing ? i : i - 2;
+                right = incrementing ? i - 2 : i;
+            }
+
+            var diff = data[left] - data[right];
+
+            if (diff < 1 || diff > 3)
+                return false;
+        }
+
+        return true;
     }
 }
