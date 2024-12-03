30db5ebf41a9b1c74a479c0fb6cbeb63b9eba717
Dirk Lemstra <dirk@lemstra.org>
2024-12-03 07:14:51 +0100

    Implemented day 3 part 2.

--- 2024/03/Day3.cs
+++ 2024/03/Day3.cs
@@ -59,7 +59,83 @@ internal class Day3 : IPuzzle
 
     public string Part2(string input)
     {
-       return string.Empty;
+        var total = 0;
+        var index = 0;
+        var stage = Stage.Start;
+        var left = -1;
+        var right = -1;
+
+        while (index != -1 && index < input.Length)
+        {
+            switch (stage)
+            {
+                case Stage.Start:
+                    left = -1;
+                    right = -1;
+
+                    var mulIndex = input.IndexOf("mul(", index);
+                    if (mulIndex == -1)
+                    {
+                        index = mulIndex;
+                    }
+                    else
+                    {
+                        var dontIndex = input.IndexOf("don't()", index);
+                        if (dontIndex != -1 && dontIndex < mulIndex)
+                        {
+                            index = dontIndex + 7;
+                            stage = Stage.Disabled;
+                        }
+                        else
+                        {
+                            index = mulIndex + 4;
+                            stage = Stage.Left;
+                        }
+                    }
+
+                    break;
+                case Stage.Disabled:
+                    index = input.IndexOf("do()", index);
+                    if (index != -1)
+                    {
+                        index += 4;
+                        stage = Stage.Start;
+                    }
+                    break;
+                case Stage.Left:
+                    if (int.TryParse(new string([input[index]]), out var leftNumber))
+                    {
+                        left = left == -1 ? 0 : left *= 10;
+                        left += leftNumber;
+                    }
+                    else if (left != -1 && input[index] == ',')
+                    {
+                        stage = Stage.Right;
+                    }
+                    else
+                    {
+                        stage = Stage.Start;
+                    }
+                    index++;
+                    break;
+                case Stage.Right:
+                    if (int.TryParse(new string([input[index]]), out var rightNumber))
+                    {
+                        right = right == -1 ? 0 : right *= 10;
+                        right += rightNumber;
+                    }
+                    else
+                    {
+                        if (right != -1 && input[index] == ')')
+                            total += left * right;
+                        stage = Stage.Start;
+                    }
+                    index++;
+                    break;
+            }
+        }
+
+        return total.ToString();
     }
 }
 
@@ -69,4 +145,5 @@ enum Stage
     Left,
     Right,
     End,
+    Disabled,
 }
