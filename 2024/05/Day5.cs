eb180bfc2e7246d36cfb38e1fc3da03e99c2cf0f
Dirk Lemstra <dirk@lemstra.org>
2024-12-05 17:45:06 +0100

    Implemented day 5 part 2.

--- 2024/05/Day5.cs
+++ 2024/05/Day5.cs
@@ -2,37 +2,11 @@ internal class Day5 : IPuzzle
 {
     public string Part1(string input)
     {
-        var before = new Dictionary<int, List<int>>();
-        var after = new Dictionary<int, List<int>>();
-        var lines = input.Split('\n');
-        var i = 0;
-        for (; i < lines.Length; i++)
-        {
-            var line = lines[i].Trim();
-            if (line.Length == 0)
-            {
-                i++;
-                break;
-            }
-
-            var info = line.Split('|').Select(int.Parse).ToArray();
-            var left = info[0];
-            var right = info[1];
-
-            if (!before.ContainsKey(right))
-                before[right] = new List<int>();
-            before[right].Add(left);
-
-            if (!after.ContainsKey(left))
-                after[left] = new List<int>();
-            after[left].Add(right);
-        }
+        var updateInfo = new UpdateInfo(input);
 
         var total = 0;
-
-        for (; i < lines.Length; i++)
+        foreach (var line in updateInfo.Data)
         {
-            var line = lines[i].Trim();
             var numbers = line.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             var isValid = true;
             for (var j = 0; j < numbers.Length && isValid; j++)
@@ -41,14 +15,14 @@ internal class Day5 : IPuzzle
                 for (var k = j + 1; k < numbers.Length && isValid; k++)
                 {
                     var otherNumber = numbers[k];
-                    if (before.TryGetValue(otherNumber, out var beforeNumbers) && !beforeNumbers.Contains(number))
+                    if (updateInfo.Before.TryGetValue(otherNumber, out var beforeNumbers) && !beforeNumbers.Contains(number))
                         isValid = false;
                 }
 
                 for (var k = 0; k < j && isValid; k++)
                 {
                     var otherNumber = numbers[k];
-                    if (after.TryGetValue(otherNumber, out var afterNumbers) && !afterNumbers.Contains(number))
+                    if (updateInfo.After.TryGetValue(otherNumber, out var afterNumbers) && !afterNumbers.Contains(number))
                         isValid = false;
                 }
             }
@@ -56,11 +30,101 @@ internal class Day5 : IPuzzle
             if (isValid)
                 total += numbers[numbers.Length / 2];
         }
+
         return total.ToString();
     }
 
     public string Part2(string input)
     {
-        return "part 2";
+        var updateInfo = new UpdateInfo(input);
+
+        var total = 0;
+        foreach (var line in updateInfo.Data)
+        {
+            var numbers = line.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var isValid = false;
+            var tries = 0;
+
+            while (isValid == false)
+            {
+                tries++;
+                isValid = true;
+                for (var j = 0; j < numbers.Length && isValid; j++)
+                {
+                    var number = numbers[j];
+                    for (var k = j + 1; k < numbers.Length && isValid; k++)
+                    {
+                        var otherNumber = numbers[k];
+                        if (updateInfo.Before.TryGetValue(otherNumber, out var beforeNumbers) && !beforeNumbers.Contains(number))
+                        {
+                            var temp = numbers[j];
+                            numbers[j] = numbers[k];
+                            numbers[k] = temp;
+                            isValid = false;
+                        }
+                    }
+
+                    for (var k = 0; k < j && isValid; k++)
+                    {
+                        var otherNumber = numbers[k];
+                        if (updateInfo.After.TryGetValue(otherNumber, out var afterNumbers) && !afterNumbers.Contains(number))
+                        {
+                            var temp = numbers[j];
+                            numbers[j] = numbers[k];
+                            numbers[k] = temp;
+                            isValid = false;
+                        }
+                    }
+                }
+            }
+
+            if (tries > 1)
+                total += numbers[numbers.Length / 2];
+        }
+
+        return total.ToString();
+    }
+
+    private class UpdateInfo
+    {
+        public IReadOnlyDictionary<int, List<int>> Before { get; }
+        public IReadOnlyDictionary<int, List<int>> After { get; }
+
+        public IReadOnlyList<string> Data;
+
+        public UpdateInfo(string input)
+        {
+            var before = new Dictionary<int, List<int>>();
+            var after = new Dictionary<int, List<int>>();
+
+            var lines = input.Split('\n');
+            var i = 0;
+            for (; i < lines.Length; i++)
+            {
+                var line = lines[i].Trim();
+                if (line.Length == 0)
+                {
+                    i++;
+                    break;
+                }
+
+                var info = line.Split('|').Select(int.Parse).ToArray();
+                var left = info[0];
+                var right = info[1];
+
+                if (!before.ContainsKey(right))
+                    before[right] = new List<int>();
+                before[right].Add(left);
+
+                if (!after.ContainsKey(left))
+                    after[left] = new List<int>();
+                after[left].Add(right);
+            }
+
+            Before = before;
+            After = after;
+
+            Data = lines.Skip(i).Select(line => line.Trim()).ToArray();
+        }
     }
 }
