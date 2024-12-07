034d91aa5089d432ea37cbd08e961c857d26d7e8
Dirk Lemstra <dirk@lemstra.org>
2024-12-07 09:07:47 +0100

    Implemented day 7 part 2.

--- 2024/07/Day7.cs
+++ 2024/07/Day7.cs
@@ -18,7 +18,18 @@ internal class Day7 : IPuzzle
 
     public string Part2(string input)
     {
-        return "Not implemented";
+        long total = 0;
+        foreach (var line in input.Split('\n'))
+        {
+            var info = line.Split(": ");
+            var number = long.Parse(info[0]);
+            var numbers = info[1].Split(" ").Select(long.Parse).ToArray();
+
+            if (IsValid2(numbers[0], 1, numbers, number))
+                total += number;
+        }
+
+        return total.ToString();
     }
 
     private bool IsValid(long number, int index, long[] numbers, long result)
@@ -33,6 +44,33 @@ internal class Day7 : IPuzzle
             return true;
 
         if (IsValid(number * numbers[index], index + 1, numbers, result))
+            return true;
+
+        return false;
+    }
+
+    private bool IsValid2(long number, int index, long[] numbers, long result)
+    {
+        if (number > result)
+            return false;
+
+        if (index == numbers.Length)
+            return number == result;
+
+        if (IsValid2(number + numbers[index], index + 1, numbers, result))
+            return true;
+
+        if (IsValid2(number * numbers[index], index + 1, numbers, result))
+            return true;
+
+        var right = numbers[index];
+        while (right != 0)
+        {
+            number *= 10;
+            right /= 10;
+        }
+
+        if (IsValid2(number + numbers[index], index + 1, numbers, result))
             return true;
 
         return false;
