df791d48681ea93a0c69c517a5c0821c361cd5ef
Dirk Lemstra <dirk@lemstra.org>
2024-12-09 21:48:33 +0100

    Implemented day 9 part 2.

--- 2024/09/Day9.cs
+++ 2024/09/Day9.cs
@@ -57,6 +57,57 @@ internal class Day9 : IPuzzle
 
     public string Part2(string input)
     {
-        return "Not implemented";
+        var numbers = input.Trim().ToCharArray().Select(c => (int)c - 48).ToArray();
+        var total = 0L;
+
+        var id = 0;
+        var sets = new List<(int, List<int>)>();
+        for (var i = 0; i < numbers.Length; i++, id++)
+        {
+            sets.Add((0, Enumerable.Repeat(id, numbers[i++]).ToList()));
+            if (i < numbers.Length)
+                sets.Add((numbers[i], new List<int>(numbers[i])));
+        }
+
+        var start = 1;
+        var end = sets.Count - 1;
+
+        for (var i = sets.Count - 1; i >= 1; i -= 2)
+        {
+            var (_, valuesToMove) = sets[i];
+            for (var j = start; j < i; j += 2)
+            {
+                var (available, values) = sets[j];
+                if (available >= valuesToMove.Count)
+                {
+                    values.AddRange(valuesToMove);
+                    available -= valuesToMove.Count;
+
+                    if (available == 0 && start == j)
+                        start = j + 2;
+
+                    if (i == end)
+                        end -= 2;
+
+                    sets[i] = (valuesToMove.Count, []);
+                    sets[j] = (available, values);
+                    break;
+                }
+            }
+        }
+        end += 4;
+
+        id = 0;
+        foreach (var (available, values) in sets)
+        {
+            foreach (var value in values)
+            {
+                total += id++ * value;
+            }
+
+            id += available;
+        }
+
+        return total.ToString();
     }
 }
