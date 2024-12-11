1dbfbc372125551fb536598093d2b15b4fc69ce4
Dirk Lemstra <dirk@lemstra.org>
2024-12-11 22:40:33 +0100

    Implemented day 11 part 2.

--- 2024/11/Day11.cs
+++ 2024/11/Day11.cs
@@ -1,41 +1,60 @@
 internal class Day11 : IPuzzle
 {
     public string Part1(string input)
+        => StonesCount(input, 25);
+
+    public string Part2(string input)
+        => StonesCount(input, 75);
+
+    private string StonesCount(string input, int times)
     {
         var items = new LinkedList<long>(input.Trim().Split(' ').Select(long.Parse));
 
-        for (var i = 0; i < 25; i++)
+        Dictionary<(long, int), long> visited = new();
+
+        var total = 0L;
+        foreach (var item in items)
         {
-            var item = items.First;
-            while (item != null)
-            {
-                if (item.Value == 0)
-                {
-                    item.Value = 1;
-                    item = item.Next;
-                    continue;
-                }
-
-                var (first, second) = SplitNumber(item.Value);
-                if (first != -1)
-                {
-                    item.Value = first;
-                    items.AddAfter(item, second);
-                    item = item.Next!.Next;
-                    continue;
-                }
-
-                item.Value *= 2024;
-                item = item.Next;
-            }
+            var count = NumberCount(visited, item, times);
+            total += count;
         }
 
-        return items.Count.ToString();
+        return total.ToString();
     }
 
-    public string Part2(string input)
+    private long NumberCount(Dictionary<(long, int), long> visited, long number, int times)
     {
-        return "Not implemented";
+        if (times == 0)
+            return 1;
+
+        var key = (number, times);
+
+        if (visited.TryGetValue(key, out var value))
+            return value;
+
+        if (number == 0)
+        {
+            var count = NumberCount(visited, 1, times - 1);
+            visited[key] = count;
+            return count;
+        }
+
+        var (first, second) = SplitNumber(number);
+        if (first != -1)
+        {
+            var count = NumberCount(visited, first, times - 1);
+            count += NumberCount(visited, second, times - 1);
+
+            visited[key] = count;
+
+            return count;
+        }
+        else
+        {
+            var count = NumberCount(visited, number * 2024, times - 1);
+            visited[key] = count;
+            return count;
+        }
     }
 
     private (long, long) SplitNumber(long number)
