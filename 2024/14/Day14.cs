d8d61c9037306b697c9fea035e86273c874109d3
Dirk Lemstra <dirk@lemstra.org>
2024-12-14 08:01:36 +0100

    Implemented day 14 part 2.

--- 2024/14/Day14.cs
+++ 2024/14/Day14.cs
@@ -14,19 +14,11 @@ internal sealed class Day14 : IPuzzle
         var centerWidth = width / 2;
         var centerHeight = height / 2;
 
-        foreach (var item in input.Split('\n'))
+        var robots = LoadRobots(input);
+        foreach (var robot in robots)
         {
-            var info = item.Split(',');
-
-            var x = int.Parse(info[0].Split('=')[1]);
-            var y = int.Parse(info[1].Split(' ')[0]);
-
-            var vX = int.Parse(info[1].Split('=')[1]);
-            var vY = int.Parse(info[2]);
-
-            x = (x + (count * vX)) % width;
-            y = (y + (count * vY)) % height;
-
+            var x = (robot.X + (count * robot.VelocityX)) % width;
+            var y = (robot.Y + (count * robot.VelocityY)) % height;
             if (x < 0) x += width;
             if (y < 0) y += height;
 
@@ -51,6 +43,71 @@ internal sealed class Day14 : IPuzzle
 
     public string Part2(string input)
     {
-        return "Not implemented";
+        var width = 101;
+        var height = 103;
+
+        var robots = LoadRobots(input);
+
+        var count = 0;
+        while (count++ < 10000)
+        {
+            HashSet<(long, long)> points = new();
+
+            var foundTree = true;
+            foreach (var robot in robots)
+            {
+                var x = (robot.X + (count * robot.VelocityX)) % width;
+                var y = (robot.Y + (count * robot.VelocityY)) % height;
+                if (x < 0) x += width;
+                if (y < 0) y += height;
+
+                if (!points.Add((x, y)))
+                {
+                    foundTree = false;
+                    break;
+                }
+            }
+
+            if (foundTree)
+                break;
+        }
+
+        return count.ToString();
+    }
+
+    private IReadOnlyList<Robot> LoadRobots(string input)
+    {
+        var robots = new List<Robot>();
+
+        foreach (var item in input.Split('\n'))
+        {
+            var info = item.Split(',');
+
+            var x = int.Parse(info[0].Split('=')[1]);
+            var y = int.Parse(info[1].Split(' ')[0]);
+
+            var vX = int.Parse(info[1].Split('=')[1]);
+            var vY = int.Parse(info[2]);
+
+            robots.Add(new(x, y, vX, vY));
+        }
+
+        return robots;
+    }
+
+    private record Robot
+    {
+        public Robot(int x, int y, int velocityX, int velocityY)
+        {
+            X = x;
+            Y = y;
+            VelocityX = velocityX;
+            VelocityY = velocityY;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int VelocityX { get; set; }
+        public int VelocityY { get; set; }
     }
 }
