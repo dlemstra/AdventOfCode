dfd2ebe71b3f602ab7025390a6a12bcc40d19a33
Dirk Lemstra <dirk@lemstra.org>
2024-12-19 23:01:37 +0100

    Implemented day 19 part 2.

--- 2024/19/Day19.cs
+++ 2024/19/Day19.cs
@@ -6,38 +6,15 @@ internal sealed class Day19 : IPuzzle
         var patterns = info[0].Trim().Split(", ").ToList();
         patterns.Sort((a, b) => b.Length.CompareTo(a.Length));
 
-        var total = 0;
-        var possible = new LinkedList<string>();
+        var total = 0L;
+        var calculated = new Dictionary<string, long>();
         for (var i = 2; i < info.Length; i++)
         {
             var towel = info[i].Trim();
-            possible.AddLast(towel);
+            var count = PossibleCount(towel, patterns, calculated);
 
-            var found = false;
-            while (possible.Count > 0)
-            {
-                var value = possible.Last();
-                possible.RemoveLast();
-
-                foreach (var pattern in patterns)
-                {
-                    if (!value.StartsWith(pattern))
-                        continue;
-
-                    var remaining = value.Substring(pattern.Length);
-                    if (remaining.Length == 0)
-                    {
-                        found = true;
-                        possible.Clear();
-                        break;
-                    }
-
-                    possible.AddLast(remaining);
-                }
-            }
-
-            if (found)
-                total++;
+            if (count != 0)
+                total += 1;
         }
 
         return total.ToString();
@@ -45,6 +22,40 @@ internal sealed class Day19 : IPuzzle
 
     public string Part2(string input)
     {
-        return "Not implemented";
+        var info = input.Split("\n");
+        var patterns = info[0].Trim().Split(", ").ToList();
+        patterns.Sort((a, b) => b.Length.CompareTo(a.Length));
+
+        var total = 0L;
+        var calculated = new Dictionary<string, long>();
+        for (var i = 2; i < info.Length; i++)
+        {
+            var towel = info[i].Trim();
+            total += PossibleCount(towel, patterns, calculated);
+        }
+
+        return total.ToString();
+    }
+
+    private long PossibleCount(string value, List<string> patterns, Dictionary<string, long> calculated)
+    {
+        if (value.Length == 0)
+            return 1;
+
+        if (calculated.TryGetValue(value, out var count))
+            return count;
+
+        foreach (var pattern in patterns)
+        {
+            if (!value.StartsWith(pattern))
+                continue;
+
+            var remaining = value.Substring(pattern.Length);
+            count += PossibleCount(remaining, patterns, calculated);
+        }
+
+        calculated.Add(value, count);
+
+        return count;
     }
 }
