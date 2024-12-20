1bdf09074a1131aab6317d2f55bf23bc2a136658
Dirk Lemstra <dirk@lemstra.org>
2024-12-21 00:06:47 +0100

    Implemented day 20 part 2.

--- 2024/20/Day20.cs
+++ 2024/20/Day20.cs
@@ -3,85 +3,89 @@ internal sealed class Day20 : Puzzle
     public override async Task<string> Part1(string input)
     {
         var (grid, startX, startY, endX, endY) = LoadGrid(input);
-        var count = StepCount(grid, startX, startY, endX, endY, int.MaxValue);
-        Console.WriteLine(count);
+        var (count, visited) = StepCount(grid, startX, startY, endX, endY);
 
-        var total = 0;
-        for (var y = 1; y < grid.Length - 1; y++)
-        {
-            for (var x = 1; x < grid[y].Length - 1; x++)
-            {
-                if (grid[y][x] != '#')
-                    continue;
-
-                var copy = CopyGrid(grid);
-                copy[y][x] = '.';
-                var newCount = StepCount(copy, startX, startY, endX, endY, count - 100);
-
-                if (newCount > 0 && newCount < count)
-                {
-                    total++;
-                    await SetIntermediateResult(total);
-                }
-            }
-        }
-
+        var total = await GetTotal(grid, visited, 2);
         return total.ToString();
     }
 
-    public override Task<string> Part2(string input)
+    public override async Task<string> Part2(string input)
     {
-        return Task.FromResult("Not implemented");
+        var (grid, startX, startY, endX, endY) = LoadGrid(input);
+        var (count, visited) = StepCount(grid, startX, startY, endX, endY);
+
+        var total = await GetTotal(grid, visited, 20);
+        return total.ToString();
     }
 
-    private char[][] CopyGrid(char[][] input)
+    private async Task<int> GetTotal(char[][] grid, IReadOnlyDictionary<(int, int), int> visited, int maxSteps)
     {
-        var result = new char[input.Length][];
-        for (var y = 0; y < input.Length; y++)
+        var total = 0;
+        foreach (var ((x, y), startCount) in visited)
         {
-            result[y] = new char[input[y].Length];
-            for (var x = 0; x < input[y].Length; x++)
+            foreach (var (dx, dy) in FindSteps(grid, x, y, maxSteps))
             {
-                result[y][x] = input[y][x];
+                if (dx == 0 && dy == 0)
+                    continue;
+
+                var xx = x + dx;
+                var yy = y + dy;
+
+                if (!visited.TryGetValue((xx, yy), out var steps))
+                    continue;
+
+                var skipped = steps - (startCount + (Math.Abs(dx) + Math.Abs(dy)));
+                if (skipped >= 100)
+                    total++;
             }
+
+            await SetIntermediateResult(total);
         }
-        return result;
+
+        return total;
     }
 
-    private int StepCount(char[][] grid, int startX, int startY, int endX, int endY, int max)
+    private static IEnumerable<(int, int)> FindSteps(char[][] grid, int startX, int startY, int steps)
     {
-        var visited = new HashSet<(int, int)>();
+        for (var dy = -steps; dy <= steps; dy++)
+        {
+            for (var dx = -(steps - Math.Abs(dy)); dx <= (steps - Math.Abs(dy)); dx++)
+                yield return (dx, dy);
+        }
+    }
 
-        var positions = new LinkedList<(int, int, int)>();
-        positions.AddLast((startX, startY, 0));
+    private static (int, IReadOnlyDictionary<(int, int), int>) StepCount(char[][] grid, int startX, int startY, int endX, int endY)
+    {
+        var visited = new Dictionary<(int, int), int>();
+
+        var positions = new Queue<(int, int, int)>();
+        positions.Enqueue((startX, startY, 0));
 
         while (positions.Count > 0)
         {
-            var (x, y, steps) = positions.First();
-            positions.RemoveFirst();
+            var (x, y, steps) = positions.Dequeue();
 
-            if (visited.Contains((x, y)))
-                continue;
+            visited[(x, y)] = steps;
 
             if (grid[y][x] == 'E')
-                return steps;
-
-            if (steps == max)
-                continue;
-
-            visited.Add((x, y));
+                return (steps, visited);
 
             foreach (var (dx, dy) in new[] { (1, 0), (-1, 0), (0, 1), (0, -1) })
             {
                 var newX = x + dx;
                 var newY = y + dy;
 
-                if (grid[newY][newX] != '#')
-                    positions.AddLast((newX, newY, steps + 1));
+                if (grid[newY][newX] == '#')
+                    continue;
+
+                if (visited.ContainsKey((newX, newY)))
+                    continue;
+
+                positions.Enqueue((newX, newY, steps + 1));
             }
         }
 
-        return -1;
+        return (-1, visited);
     }
 
     private static (char[][], int, int, int, int) LoadGrid(string input)
