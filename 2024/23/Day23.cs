800788456c3d398d34425c0320c2f08967e4f087
Dirk Lemstra <dirk@lemstra.org>
2024-12-23 22:13:13 +0100

    Implemented day 23 part 2.

--- 2024/23/Day23.cs
+++ 2024/23/Day23.cs
@@ -4,26 +4,7 @@ internal sealed class Day23 : Puzzle
 {
     public override Task<string> Part1(string input)
     {
-        var connections = new Dictionary<string, List<string>>();
-        foreach (var line in input.Split("\n"))
-        {
-            var info = line.Trim().Split('-');
-            if (info.Length != 2)
-                continue;
-
-            var left = info[0];
-            var right = info[1];
-
-            if (!connections.TryGetValue(left, out var leftList))
-                leftList = [];
-            leftList.Add(right);
-            connections[left] = leftList;
-
-            if (!connections.TryGetValue(right, out var rightList))
-                rightList = [];
-            rightList.Add(left);
-            connections[right] = rightList;
-        }
+        var connections = LoadConnections(input);
 
         var possible = new HashSet<string>();
         foreach (var connection in connections.Keys)
@@ -50,6 +31,73 @@ internal sealed class Day23 : Puzzle
 
     public override Task<string> Part2(string input)
     {
-        return Task.FromResult("Not implemented");
+        var connections = new List<List<string>>();
+        foreach (var (name, connection) in LoadConnections(input))
+        {
+            var items = new List<string>(connection);
+            items.Add(name);
+
+            connections.Add(items);
+        }
+
+        var best = "";
+        for (var i = 0; i < connections.Count; i++)
+        {
+            var list = new List<string>(connections[i]);
+
+            var possible = new Dictionary<string, int>();
+            for (var j = 0; j < connections.Count; j++)
+            {
+                if (i == j)
+                    continue;
+
+                var uniqueItems = connections[i].Intersect(connections[j]).ToList();
+                if (uniqueItems.Count * 3 < best.Length + 1)
+                    continue;
+
+                uniqueItems.Sort();
+                var key = string.Join(",", uniqueItems);
+                if (key.Length < best.Length)
+                    continue;
+
+                possible.TryGetValue(key, out var count);
+                count++;
+                possible[key] = count;
+            }
+
+            foreach (var (pass, count) in possible)
+            {
+                if (pass.Length > best.Length && count * 3 == pass.Length - 2)
+                    best = pass;
+            }
+        }
+
+        return Task.FromResult(best);
+    }
+
+    private static IReadOnlyDictionary<string, List<string>> LoadConnections(string input)
+    {
+        var connections = new Dictionary<string, List<string>>();
+        foreach (var line in input.Split("\n"))
+        {
+            var info = line.Trim().Split('-');
+            if (info.Length != 2)
+                continue;
+
+            var left = info[0];
+            var right = info[1];
+
+            if (!connections.TryGetValue(left, out var leftList))
+                leftList = [];
+            leftList.Add(right);
+            connections[left] = leftList;
+
+            if (!connections.TryGetValue(right, out var rightList))
+                rightList = [];
+            rightList.Add(left);
+            connections[right] = rightList;
+        }
+
+        return connections;
     }
 }
