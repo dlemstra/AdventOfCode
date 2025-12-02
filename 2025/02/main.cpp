b666178c09ebbb69b59c758b3a3ef97ff6f1deab
Dirk Lemstra <dirk@lemstra.org>
2025-12-02 19:57:13 +0100

    Implemented day 2 part 2.

--- 2025/02/main.cpp
+++ 2025/02/main.cpp
@@ -21,6 +21,7 @@ int main() {
     std::string line;
 
     long part1 = 0;
+    long part2 = 0;
     while (std::getline(file, line))
     {
         auto ranges = split(line, ',');
@@ -31,17 +32,43 @@ int main() {
             for (auto i = start; i <= end; ++i)
             {
                 auto value = std::to_string(i);
+                auto len = value.length() / 2;
                 if (value.length() % 2 == 0)
                 {
-                    auto len = value.length() / 2;
                     if (value.substr(0,len) == value.substr(len,len))
                         part1 += i;
                 }
+
+                auto isValid = true;
+                for (auto j = 1; j <= len; ++j)
+                {
+                    if (value.length() % j != 0)
+                        continue;
+
+                    isValid = false;
+                    auto k = 0;
+                    while (k + j < value.length())
+                    {
+                        if (value.substr(k, j) != value.substr(k + j, j))
+                        {
+                            isValid = true;
+                            break;
+                        }
+                        k += j;
+                    }
+
+                    if (!isValid)
+                        break;
+                }
+
+                if (!isValid)
+                    part2 += i;
             }
         }
     }
 
     std::cout << part1 << std::endl;
+    std::cout << part2 << std::endl;
 
     return 0;
 }
