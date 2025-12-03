5dfae76562c4201631e5f3de3ad0061f15c01b64
Dirk Lemstra <dirk@lemstra.org>
2025-12-03 22:38:33 +0100

    Implemented day 3 part 2

--- 2025/03/main.cpp
+++ 2025/03/main.cpp
@@ -1,11 +1,32 @@
 #include <fstream>
 #include <iostream>
 
+#include <algorithm>
+
+long findPart2(const std::string& s) {
+    std::string result;
+    auto n = s.length();
+
+    for (auto i = 0; i < n && result.length() < 12; i++)
+    {
+        auto remaining_needed = 12 - result.length();
+        auto window_end = n - remaining_needed + 1;
+
+        auto max_char = *std::max_element(s.begin() + i, s.begin() + window_end);
+
+        if (s[i] == max_char)
+            result += s[i];
+    }
+
+    return std::stol(result);
+}
+
 int main() {
     std::ifstream file("input");
     std::string line;
 
     long part1 = 0;
+    long part2 = 0;
     while (std::getline(file, line))
     {
         int best = 0;
@@ -13,15 +34,18 @@ int main() {
         {
             for (auto j = line.length() - 1; j > i; j--)
             {
-                int value = ((line[i] - '0') * 10) + (line[j] - '0');
+                auto value = ((line[i] - '0') * 10) + (line[j] - '0');
                 if (value > best)
                     best = value;
             }
         }
+
         part1 += best;
+        part2 += findPart2(line);
     }
 
     std::cout << part1 << std::endl;
+    std::cout << part2 << std::endl;
 
     return 0;
 }
