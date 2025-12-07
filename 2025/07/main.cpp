c9334cdd9296d99c09064cd55d9bea1449b62dd6
Dirk Lemstra <dirk@lemstra.org>
2025-12-07 14:02:09 +0100

    Implemented day 7 part 2.

--- 2025/07/main.cpp
+++ 2025/07/main.cpp
@@ -2,12 +2,40 @@
 #include <iostream>
 
 #include <vector>
+#include <map>
+
+static long countTimelines(const std::vector<std::vector<char>>& grid, int x, int y, std::map<std::pair<int, int>, long>& cache)
+{
+    if (y >= grid.size())
+        return 1;
+
+    auto key = std::make_pair(x, y);
+    if (cache.find(key) != cache.end())
+        return cache[key];
+
+    auto count = 0L;
+    auto value = grid[y][x];
+
+    if (value == '|' || value== 'S')
+        count = countTimelines(grid, x, y + 1, cache);
+    else if (value == '^')
+    {
+        if (x > 0)
+            count = countTimelines(grid, x - 1, y + 1, cache);
+        if (x < grid[y].size() - 1)
+            count += countTimelines(grid, x + 1, y + 1, cache);
+    }
+
+    cache[key] = count;
+    return count;
+}
 
 int main() {
     std::ifstream file("input");
     std::string line;
 
     long part1 = 0;
+    long part2 = 0;
     std::vector<std::vector<char>> grid;
     while (std::getline(file, line))
     {
@@ -38,7 +66,13 @@ int main() {
         }
     }
 
-    std::cout << part1 << std::endl;
+    std::map<std::pair<int, int>, long> cache;
+    for (auto x = 0; x < grid[0].size(); x++)
+        if (grid[0][x] == 'S')
+            part2 = countTimelines(grid, x, 0, cache);
+
+    std::cout << "Part 1: " << part1 << std::endl;
+    std::cout << "Part 2: " << part2 << std::endl;
 
     return 0;
 }
